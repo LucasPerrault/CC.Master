e3a0b6850dbf8bc01ab48949cf69a717f2eebb3c
Alban Granger <agranger@lucca.fr>
2020-08-10 12:02:18 +0200

    ADD auth config

---
+++ back/src/CloudControl.Shared.Infra/Configuration/AuthenticationConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CloudControl.Shared.Infra.Configuration
+{
+    public class AuthenticationConfiguration
+    {
+        public Uri ServerUri { get; set; }
+
+        public string EndpointPath { get; set; }
+    }
+}
--- back/src/CloudControl.Shared.Infra/Configuration/Configuration.cs
+++ back/src/CloudControl.Shared.Infra/Configuration/Configuration.cs
@@ -7,6 +7,8 @@ namespace CloudControl.Shared.Infra.Configuration
 		public const string LuccaLoggerOptionsKey = "LuccaLoggerOptions";
 		public const string AppName = "CloudControl";
 
+		public AuthenticationConfiguration Authentication { get; set; }
+		public ApiKeysConfiguration ApiKeys { get; set; }
 		public LegacyCloudControlConfiguration LegacyCloudControl { get; set; }
 	}
 }
