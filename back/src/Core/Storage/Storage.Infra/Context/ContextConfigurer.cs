6533d0c56ccd1599f9026b56dbe3a13db2002fd1
boboch86 <40466697+boboch86@users.noreply.github.com>
2020-09-16 12:10:58 +0200

    ADD detailed errors

--- back/src/Core/Storage/Storage.Infra/Context/ContextConfigurer.cs
+++ back/src/Core/Storage/Storage.Infra/Context/ContextConfigurer.cs
@@ -1,6 +1,5 @@
 ﻿using Lucca.Core.AspNetCore.Tenancy;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,11 +10,21 @@ namespace Storage.Infra.Context
 		public static void ConfigureContext<TContext>(this IServiceCollection services, IWebHostEnvironment environment)
 			where TContext : CloudControlDbContext<TContext>
 		{
-			services.AddDbContextOnTenant<TContext>(DatabaseMode.MultiTenant, sqlOptions =>
-			{
-				sqlOptions
-					.EnableRetryOnFailure(5);
-			});
+			services.AddDbContextOnTenant<TContext>(
+				DatabaseMode.MultiTenant,
+				(sqlOptions) =>
+				{
+					sqlOptions.EnableRetryOnFailure(5);
+				},
+				(dbContextOptions) =>
+                {
+					if (!environment.IsProduction())
+					{
+						dbContextOptions.EnableDetailedErrors();
+						dbContextOptions.EnableSensitiveDataLogging();
+					}
+				}
+			);
 		}
 	}
 }
