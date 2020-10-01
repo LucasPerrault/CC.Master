d8e877763e408c3a22373e32ab7ebd62035516e4
versinia <angelin.versini@gmail.com>
2020-10-01 16:38:56 +0200

    MOD : ignore api key primary key, ensure current principal is api key

--- back/Core/Rights/Rights.Infra/Remote/ApiKeyPermissionsRemoteService.cs
+++ back/Core/Rights/Rights.Infra/Remote/ApiKeyPermissionsRemoteService.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using Authentication.Domain;
+using Newtonsoft.Json;
 using Partenaires.Infra.Services;
 using Rights.Infra.Models;
 using Rights.Infra.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -12,22 +14,31 @@ namespace Rights.Infra.Remote
 {
 	public class ApiKeyPermissionsRemoteService : PartenairesService
 	{
-		public ApiKeyPermissionsRemoteService(HttpClient httpClient, JsonSerializer jsonSerializer, ClaimsPrincipal claimsPrincipal)
+		public ApiKeyPermissionsRemoteService
+			(HttpClient httpClient, JsonSerializer jsonSerializer, ClaimsPrincipal claimsPrincipal)
 			: base(httpClient, jsonSerializer, claimsPrincipal)
 		{ }
 
 		internal async Task<IReadOnlyCollection<ApiKeyPermission>> GetApiKeyPermissionsAsync(int apiKeyId)
 		{
+			// apiKeyId is not known (Lucca apis hide it by design)
+			// calling as current api key principal
+			// remote instance will filter results accordingly
+			// https://github.com/LuccaSA/ilucca/blob/e5a6707565918cfb09388717344a60ce9e1321e6/Domain/Lucca.Domain/Repositories/ForeignAppPermissionsLookupRepository.cs#L40
+			if (!(_claimsPrincipal is CloudControlApiKeyClaimsPrincipal))
+			{
+				throw new ApplicationException("Method assumes ClaimsPrincipal is an api key, but it is not");
+			}
+
 			var queryParams = new Dictionary<string, string>
 			{
 				{ "appInstanceId", RightsHelper.CloudControlAppInstanceId.ToString() },
-				{ "foreignAppId", apiKeyId.ToString() },
 				{ "fields", ApiKeyPermission.ApiFields }
 			};
 
-			var apiKeyPermissionsReponse = await GetObjectCollectionResponseAsync<ApiKeyPermission>(queryParams);
+			var apiKeyPermissionsResponse = await GetObjectCollectionResponseAsync<ApiKeyPermission>(queryParams);
 
-			var allApiKeyPermissions = apiKeyPermissionsReponse.Data.Items;
+			var allApiKeyPermissions = apiKeyPermissionsResponse.Data.Items;
 
 			return allApiKeyPermissions.ToList();
 		}
