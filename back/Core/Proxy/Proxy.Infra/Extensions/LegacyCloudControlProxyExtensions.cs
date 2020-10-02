b498ab9feab79dac4be2b919b61742fcaea3545b
versinia <angelin.versini@gmail.com>
2020-10-02 12:27:59 +0200

    MOD manually copy lucca-specific XForwardrd headers

--- back/Core/Proxy/Proxy.Infra/Extensions/LegacyCloudControlProxyExtensions.cs
+++ back/Core/Proxy/Proxy.Infra/Extensions/LegacyCloudControlProxyExtensions.cs
@@ -1,4 +1,4 @@
-using Core.Proxy.Infra.Configuration;
+﻿using Core.Proxy.Infra.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +11,7 @@ namespace Core.Proxy.Infra.Extensions
 {
 	public static class LegacyCloudControlProxyExtensions
 	{
+		private const string FORWARDED_BY_LUCCA_HEADER = "X-Forwarded-By-Lucca";
 		private const string FORWARDED_BY_CC_MASTER_HEADER = "X-Forwarded-By-CC-Master";
 
 		public static IApplicationBuilder UseLegacyCloudControlHttpProxy(this IApplicationBuilder app)
@@ -50,6 +51,10 @@ namespace Core.Proxy.Infra.Extensions
 
 		private static ForwardContext AddXForwardedCustomHeaders(this ForwardContext forwardContext, HttpContext context)
 		{
+			if (context.Request.Headers.TryGetValue(FORWARDED_BY_LUCCA_HEADER, out var forwardedByLuccaHeader))
+			{
+				forwardContext.UpstreamRequest.Headers.Add(FORWARDED_BY_LUCCA_HEADER, new [] { true.ToString()});
+			}
 			forwardContext.UpstreamRequest.Headers.Add(FORWARDED_BY_CC_MASTER_HEADER, new [] { true.ToString()});
 			return forwardContext;
 		}
