4bb0fc4fa8c2ac4d01f4f336753b75cd17b7a0f6
Alban Granger <agranger@lucca.fr>
2020-12-01 16:37:53 +0100

    ADD legacy cc clients service

---
+++ back/Billing/Billing.Contracts/Billing.Contracts.Infra/Legacy/LegacyCcClientsRemoteService.cs
@@ -0,0 +1,23 @@
+﻿using Billing.Contracts.Domain.Clients.Interfaces;
+using Core.Proxy.Infra.Services;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Billing.Contracts.Infra.Legacy
+{
+    public class LegacyClientsRemoteService : LegacyCcAuthenticatedRemoteService, ILegacyClientsRemoteService
+    {
+        public LegacyClientsRemoteService(HttpClient httpClient, JsonSerializer jsonSerializer, ClaimsPrincipal principal)
+            : base(httpClient, jsonSerializer, principal)
+        { }
+
+        public Task SyncAsync()
+        {
+            var queryParams = new Dictionary<string, string>();
+            return GetObjectResponseAsync<object>("sync", queryParams);
+        }
+    }
+}
