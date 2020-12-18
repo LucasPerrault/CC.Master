267159fe12e885ecb85717936e4a54863292e2ef
Perrault <lucas.perrault@hotmail.com>
2020-12-18 17:26:23 +0100

    ADD cc as prefix for selector and directive

--- front/cc-master/src/app/app.component.ts
+++ front/cc-master/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
+	selector: 'cc-root',
 	templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
