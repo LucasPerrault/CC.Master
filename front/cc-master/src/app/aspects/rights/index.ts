7d543bd979d6bb9e5f267e6e1ef43cc93f4b8147
Perrault <lucas.perrault@hotmail.com>
2020-12-18 19:26:40 +0100

    ADD rights module

--- front/cc-master/src/app/aspects/rights/index.ts
+++ front/cc-master/src/app/aspects/rights/index.ts
@@ -1,5 +1,7 @@
 export * from './enums/operation.enum';
 export * from './models/permission.interface';
 export * from './services/rights.service';
+export { RightsModule } from './rights.module';
+
 
 
---
+++ front/cc-master/src/app/aspects/rights/rights.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RightsService} from '@cc/aspects/rights/services/rights.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    RightsService,
+  ]
+})
+export class RightsModule { }
