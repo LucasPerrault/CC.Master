e1a10be44a9ed8ee71acc55bc72d57e3bd1d6f41
Perrault <lucas.perrault@hotmail.com>
2020-12-18 19:59:20 +0100

    ADD rights module in app

--- front/cc-master/src/app/app.module.ts
+++ front/cc-master/src/app/app.module.ts
@@ -1,31 +1,34 @@
-import { registerLocaleData } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import localeFr from '@angular/common/locales/fr';
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { PrincipalModule } from './aspects/principal';
+import {registerLocaleData} from '@angular/common';
+import {AppComponent} from './app.component';
+import {PrincipalModule} from './aspects/principal';
+import {RightsModule} from '@cc/aspects/rights/rights.module';
+import {ForbiddenComponent, forbiddenUrl} from '@cc/common/errors';
 
 registerLocaleData(localeFr);
 
 const routes: Routes = [
-	{ path: 'logs', loadChildren: () => import('./pages/logs').then(m => m.LogsModule) },
-	{ path: '**', redirectTo: 'logs', pathMatch: 'full' },
+  { path: 'logs', loadChildren: () => import('./pages/logs').then(m => m.LogsModule)},
+  {path: '**', redirectTo: 'logs', pathMatch: 'full'},
 ];
 
 @NgModule({
-	declarations: [
-		AppComponent,
-	],
-	imports: [
-		BrowserModule,
-		RouterModule.forRoot(routes),
-		PrincipalModule.forRoot(),
-	],
-	providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' },
+  declarations: [
+    AppComponent,
   ],
-	bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    PrincipalModule.forRoot(),
+    RightsModule,
+  ],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr-FR'},
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
