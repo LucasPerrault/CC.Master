ea63fa6784dc0e27b33644a9f87356d463c1ccdd
Perrault <lucas.perrault@hotmail.com>
2020-12-21 11:27:04 +0100

    ADD translate module and delete the fixed local id

--- front/cc-master/src/app/app.component.ts
+++ front/cc-master/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, LOCALE_ID, OnInit} from '@angular/core';
+import {TranslateInitializationService} from '@cc/aspects/translate';
 
 @Component({
 	selector: 'cc-root',
 	templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-	constructor() {}
+	constructor(
+    @Inject(LOCALE_ID) private localeId: string,
+	  private translateService: TranslateInitializationService
+  ) {
+	  this.translateService.initializeTranslations(localeId);
+  }
+
 	ngOnInit(): void {}
 }
--- front/cc-master/src/app/app.module.ts
+++ front/cc-master/src/app/app.module.ts
@@ -3,11 +3,10 @@ import localeFr from '@angular/common/locales/fr';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
+import {NgModule} from '@angular/core';
+import {PrincipalModule} from '@cc/aspects/principal';
+import {TranslateModule} from '@cc/aspects/translate';
 import { AppComponent } from './app.component';
-import { PrincipalModule } from './aspects/principal';
-
-registerLocaleData(localeFr);
 
 const routes: Routes = [
 	{ path: 'logs', loadChildren: () => import('./pages/logs').then(m => m.LogsModule) },
@@ -22,9 +21,9 @@ const routes: Routes = [
 		BrowserModule,
 		RouterModule.forRoot(routes),
 		PrincipalModule.forRoot(),
+    TranslateModule.forRoot(),
 	],
 	providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' },
   ],
 	bootstrap: [AppComponent],
 })
