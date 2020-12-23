338be5d391012728d1e87f3eb6ea80bac2dfffbe
Perrault <lucas.perrault@hotmail.com>
2020-12-23 16:46:40 +0100

    ADD translation for errorPages

--- front/cc-master/src/app/common/errors/errors.module.ts
+++ front/cc-master/src/app/common/errors/errors.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@cc/aspects/translate';
 import { ForbiddenComponent } from '@cc/common/errors/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [ForbiddenComponent],
   imports: [
     CommonModule,
+    TranslateModule,
   ],
   providers: [],
 })
