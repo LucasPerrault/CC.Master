3ad1bb80020f380edbcc723b5cc5cc0c97656576
Perrault <lucas.perrault@hotmail.com>
2021-01-22 17:32:49 +0100

    ADD route param filters for date range

--- front/cc-master/src/app/common/queries/constants/api-date.constant.ts
+++ front/cc-master/src/app/common/queries/constants/api-date.constant.ts
@@ -1,1 +1,5 @@
 export const apiDateFormat = 'yyyy-MM-dd';
+
+export const apiDateBetweenKeyword = 'between';
+export const apiDateSinceKeyword = 'since';
+export const apiDateUntilKeyword = 'until';
--- front/cc-master/src/app/pages/logs/components/date-range-select/date-range-select.component.ts
+++ front/cc-master/src/app/pages/logs/components/date-range-select/date-range-select.component.ts
@@ -1,26 +1,64 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { apiDateFormat } from '@cc/common/queries';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { apiDateBetweenKeyword, apiDateFormat, apiDateSinceKeyword, apiDateUntilKeyword } from '@cc/common/queries';
 import { LuNativeDateAdapter } from '@lucca-front/ng/core';
 
 @Component({
   selector: 'cc-date-range-select',
   templateUrl: './date-range-select.component.html',
 })
-export class DateRangeSelectComponent {
+export class DateRangeSelectComponent implements OnInit {
   @Output() public dateRangeToString: EventEmitter<string> = new EventEmitter<string>();
 
   public startDate;
   public endDate;
   public todayDate = new Date();
 
-  constructor(private adapter: LuNativeDateAdapter) {}
+  private routerParamKey = 'date';
 
-  public updateDatesSelected(startDate: Date, endDate: Date) {
-    const dateRangeToApiQueryString = this.getDateRangeToQueryString(startDate, endDate);
-    this.dateRangeToString.emit(dateRangeToApiQueryString);
+  constructor(private adapter: LuNativeDateAdapter, private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
-  public getDateRangeToQueryString(startDate: Date, endDate: Date): string {
+  public ngOnInit() {
+    const routerParamValue = this.activatedRoute.snapshot.queryParamMap.get(this.routerParamKey);
+    if (!routerParamValue) {
+      return;
+    }
+
+    this.dateRangeToString.emit(routerParamValue);
+    this.setDatesWithQueryString(routerParamValue.split(','));
+  }
+
+  public async updateDatesSelectedAsync(startDate: Date, endDate: Date): Promise<void> {
+    const dateRangeToApiQueryString = this.getDateRangeToQueryString(startDate, endDate);
+    this.dateRangeToString.emit(dateRangeToApiQueryString);
+
+    await this.updateRouterAsync(dateRangeToApiQueryString);
+  }
+
+  private setDatesWithQueryString(queryParams: string[]): void {
+    const dateKeyword = queryParams.shift();
+    if (!dateKeyword) {
+      return;
+    }
+
+    if (dateKeyword === apiDateBetweenKeyword) {
+      this.startDate = new Date(queryParams.shift());
+      this.endDate = new Date(queryParams.shift());
+      return;
+    }
+
+    if (dateKeyword === apiDateSinceKeyword) {
+      this.startDate = new Date(queryParams.shift());
+      return;
+    }
+
+    if (dateKeyword === apiDateUntilKeyword) {
+      this.endDate = new Date(queryParams.shift());
+    }
+  }
+
+  private getDateRangeToQueryString(startDate: Date, endDate: Date): string {
     if (!startDate && !endDate) {
       return;
     }
@@ -29,20 +67,30 @@ export class DateRangeSelectComponent {
     const endDateFormat = this.getDateToApiQueryString(endDate);
 
     if (!endDateFormat) {
-      return `since,${startDateFormat}`;
+      return `${apiDateSinceKeyword},${startDateFormat}`;
     }
     if (!startDateFormat) {
-      return `until,${endDateFormat}`;
+      return `${apiDateUntilKeyword},${endDateFormat}`;
     }
 
-    return `between,${startDateFormat},${endDateFormat}`;
+    return `${apiDateBetweenKeyword},${startDateFormat},${endDateFormat}`;
   }
 
-  public getDateToApiQueryString(date: Date): string {
+  private getDateToApiQueryString(date: Date): string {
     if (!date) {
       return;
     }
 
     return this.adapter.format(date, apiDateFormat);
+  }
+
+  private async updateRouterAsync(value: string): Promise<void> {
+    const queryParams = { [this.routerParamKey]: !!value ? value : null };
+
+    await this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams,
+      queryParamsHandling: 'merge',
+    });
   }
 }
