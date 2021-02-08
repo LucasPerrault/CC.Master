c57479c40834ee15906ead2fbf2830e5d752e819
Perrault <lucas.perrault@hotmail.com>
2021-02-08 19:39:03 +0100

    DEL useless modules

--- front/cc-master/src/app/pages/logs/logs.module.ts
+++ front/cc-master/src/app/pages/logs/logs.module.ts
@@ -13,19 +13,8 @@ import {
   UserApiSelectModule,
 } from '@cc/common/forms';
 import { PagingModule } from '@cc/common/paging';
-import { LuApiModule } from '@lucca-front/ng/api';
-import { LuDateModule } from '@lucca-front/ng/date';
-import { LuInputModule } from '@lucca-front/ng/input';
-import {
-  LuOptionFeederModule,
-  LuOptionModule,
-  LuOptionPagerModule,
-  LuOptionPickerModule,
-  LuOptionSearcherModule,
-} from '@lucca-front/ng/option';
-import { LuSelectInputModule } from '@lucca-front/ng/select';
+import { EnvironmentsModule } from '@cc/domain/environments';
 
-import { EnvironmentsModule } from '../../domain/environments';
 import { AnonymizationButtonGroupComponent } from './components/anonymization-button-group/anonymization-button-group.component';
 import { LogsFiltersComponent } from './components/logs-filter/logs-filter.component';
 import { LogsListComponent } from './components/logs-list/logs-list.component';
@@ -44,27 +33,18 @@ const routes: Routes = [
 	],
   imports: [
     RouterModule.forChild(routes),
-    ScrollingModule,
     CommonModule,
-    LuSelectInputModule,
     FormsModule,
-    LuOptionPickerModule,
-    LuOptionFeederModule,
-    LuOptionSearcherModule,
-    LuOptionPagerModule,
-    LuOptionModule,
-    LuInputModule,
-    LuDateModule,
-    LuApiModule,
-    TranslateModule,
     EnvironmentsModule,
-    PagingModule,
-    FiltersModule,
     EnvironmentApiSelectModule,
     EnvironmentDomainSelectModule,
-    UserApiSelectModule,
     EnvironmentActionSelectModule,
+    UserApiSelectModule,
     DateRangeSelectModule,
+    ScrollingModule,
+    PagingModule,
+    TranslateModule,
+    FiltersModule,
   ],
 })
 export class LogsModule {};
