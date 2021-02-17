680ea0371d65ca6cd4b962965eccdec6bab7af08
Perrault <lucas.perrault@hotmail.com>
2021-02-17 11:35:58 +0100

    MOD return http response type

--- front/cc-master/src/app/common/queries/models/http-api-v3-response.interface.ts
+++ front/cc-master/src/app/common/queries/models/http-api-v3-response.interface.ts
@@ -6,7 +6,13 @@ export interface IHttpApiV3Collection<T> {
   items: T[];
 }
 
-export interface IHttpApiV3CollectionCountResponse<T> extends IHttpApiV3Collection<T> {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IHttpApiV3CollectionResponse<T> extends IHttpApiV3Response<IHttpApiV3Collection<T>> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IHttpApiV3CollectionCountResponse<T> extends IHttpApiV3Response<IHttpApiV3CollectionCount<T>> {}
+
+
+export interface IHttpApiV3CollectionCount<T> extends IHttpApiV3Collection<T> {
   count: number;
 }
 
--- front/cc-master/src/app/domain/environments/services/logs.service.ts
+++ front/cc-master/src/app/domain/environments/services/logs.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IHttpApiV3CollectionCountResponse, IHttpApiV3Response } from '@cc/common/queries';
+import { IHttpApiV3CollectionCount, IHttpApiV3CollectionCountResponse } from '@cc/common/queries';
 import { IEnvironmentLog } from '@cc/domain/environments';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,12 +11,12 @@ export class LogsService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getLogs$(httpParams: HttpParams): Observable<IHttpApiV3CollectionCountResponse<IEnvironmentLog>> {
+  public getLogs$(httpParams: HttpParams): Observable<IHttpApiV3CollectionCount<IEnvironmentLog>> {
     const fields = 'collection.count,id,name,user,isAnonymizedData,activity,createdOn,environment[subDomain,domainName],' +
       'messages[id,message,type]';
     const environmentLogUrl = `/api/v3/environmentLogs`;
 
-    return this.httpClient.get<IHttpApiV3Response<IHttpApiV3CollectionCountResponse<IEnvironmentLog>>>(environmentLogUrl, {
+    return this.httpClient.get<IHttpApiV3CollectionCountResponse<IEnvironmentLog>>(environmentLogUrl, {
       params: httpParams.set('fields', fields),
     }).pipe(map(response => response.data));
   }
