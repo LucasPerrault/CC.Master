25a9f4ad1f12918114719dd1d8dd249ccbd97624
Perrault <lucas.perrault@hotmail.com>
2021-02-17 12:26:00 +0100

    MOD sort queries methods
    - fix toHttpParams if sortParams are undefined or empty.
    - add constants for apiV3Key

--- front/cc-master/src/app/common/queries/constants/api-sort.constant.ts
+++ front/cc-master/src/app/common/queries/constants/api-sort.constant.ts
@@ -2,6 +2,10 @@ import { HttpParams } from '@angular/common/http';
 import { ApiV3Order, IApiV3SortParams } from '@cc/common/queries';
 import { ISortParams, SortOrder } from '@cc/common/sort';
 
+export const apiV3SortKey = 'orderBy';
+export const apiV3SortOrderAscendingKey = 'asc';
+export const apiV3SortOrderDescendingKey = 'desc';
+
 export const toApiV3SortParams = (sortParams: ISortParams[]): IApiV3SortParams[] =>
   sortParams.map(param => ({
     field: param.field,
@@ -9,12 +13,16 @@ export const toApiV3SortParams = (sortParams: ISortParams[]): IApiV3SortParams[]
   }));
 
 export const getApiV3OrderPrefix = (order: SortOrder): ApiV3Order =>
-  order === SortOrder.Asc ? 'asc' : 'desc';
+  order === SortOrder.Asc ? apiV3SortOrderAscendingKey : apiV3SortOrderDescendingKey;
 
 
 export const apiV3SortToHttpParams = (params: HttpParams, sortParams: IApiV3SortParams[]): HttpParams => {
+  if (!sortParams || !sortParams.length) {
+    return params;
+  }
+
   const formattedParams = sortParams.map(sort => `${sort.field},${sort.order}`);
-  return params.set('orderBy', formattedParams.join(','));
+  return params.set(apiV3SortKey, formattedParams.join(','));
 };
 
 
