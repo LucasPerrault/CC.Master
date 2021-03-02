7b0daa4b1eebc79aa3f5d117bbbcd4e9836200bf
Perrault <lucas.perrault@hotmail.com>
2021-03-02 14:06:07 +0100

    DEL useless code
    It's not mandatory to implement this methods

--- front/cc-master/src/app/common/forms/select/environment-api-select/environment-api-select.service.ts
+++ front/cc-master/src/app/common/forms/select/environment-api-select/environment-api-select.service.ts
@@ -1,43 +1,11 @@
 import { Injectable } from '@angular/core';
-import { defaultPagingParams } from '@cc/common/paging';
 import { IEnvironment } from '@cc/domain/environments';
 import { LuApiV3Service } from '@lucca-front/ng/api';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class EnvironmentApiSelectService extends LuApiV3Service<IEnvironment> {
-
-  public getAll(filters?: string[]): Observable<IEnvironment[]> {
-    return this._get([this.url, ...filters].join('&'));
-  }
-
-  public getPaged(page: number, filters: string[] = []): Observable<IEnvironment[]> {
-    const url = [this.url, this.getPaging(page), ...filters].join('&');
-    return this._get(url);
-  }
-
-  public searchAll(clue: string, filters: string[] = []): Observable<IEnvironment[]> {
-    if (!clue) {
-      return this.getAll(filters);
-    }
-    const url = [this.url, this._clueFilter(clue), ...filters].join('&');
-    return this._get(url);
-  }
-
-  public searchPaged(clue: string, page: number, filters: string[] = []): Observable<IEnvironment[]> {
-    if (!clue) {
-      return this.getPaged(page, filters);
-    }
-    const url = [this.url, this._clueFilter(clue), this.getPaging(page), ...filters].join('&');
-    return this._get(url);
-  }
-
   // eslint-disable-next-line @typescript-eslint/naming-convention
   protected _clueFilter(clue: string): string {
     return `subdomain=like,${clue}`;
-  }
-
-  private getPaging(page: number): string {
-    return `paging=${page * defaultPagingParams.limit},${defaultPagingParams.limit}`;
   }
 }
