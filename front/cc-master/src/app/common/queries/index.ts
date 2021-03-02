ba44f8453de61fd86b6af55b4af5edda54c145b1
Perrault <lucas.perrault@hotmail.com>
2021-03-02 11:23:37 +0100

    DEL useless file

--- front/cc-master/src/app/common/queries/index.ts
+++ front/cc-master/src/app/common/queries/index.ts
@@ -3,4 +3,3 @@ export * from './constants/api-sort.constant';
 export * from './models/api-v3-paging-params.interface';
 export * from './models/api-v3-sort-params.interface';
 export * from './models/http-api-v3-response.interface';
-export * from './models/http-query-params.interface';
--- front/cc-master/src/app/common/queries/models/http-query-params.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IHttpQueryParams {
-  [key: string]: string;
-}
