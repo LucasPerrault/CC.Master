09e4874a023f9165f60ad8d54b4ed13c78b21b0a
Perrault <lucas.perrault@hotmail.com>
2021-03-02 09:33:24 +0100

    ADD logs routing

---
+++ front/cc-master/src/app/pages/logs/models/logs-routing-params.interface.ts
@@ -0,0 +1,8 @@
+export interface ILogsRoutingParams {
+  environmentIds?: string;
+  domainIds?: string;
+  userIds?: string;
+  actionIds?: string;
+  createdOn?: string;
+  isAnonymized?: string;
+}
