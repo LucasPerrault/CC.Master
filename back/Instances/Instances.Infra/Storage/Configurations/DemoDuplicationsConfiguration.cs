0be47528bf7640f8db1f9e366e3507428d71ccf7
versinia <angelin.versini@gmail.com>
2021-03-26 09:46:23 +0100

    ADD not null constraints on demo duplication

--- back/Instances/Instances.Infra/Storage/Configurations/DemoDuplicationsConfiguration.cs
+++ back/Instances/Instances.Infra/Storage/Configurations/DemoDuplicationsConfiguration.cs
@@ -15,25 +15,38 @@ namespace Instances.Infra.Storage.Configurations
 
             builder.Property(d => d.Subdomain)
                 .HasColumnName("subdomain")
-                .HasMaxLength(SubdomainValidator.SubdomainMaxLength);
+                .HasMaxLength(SubdomainValidator.SubdomainMaxLength)
+                .IsRequired();
 
             builder.Property(d => d.SourceDemoSubdomain)
                 .HasColumnName("sourceDemoSubdomain")
-                .HasMaxLength(SubdomainValidator.SubdomainMaxLength);
+                .HasMaxLength(SubdomainValidator.SubdomainMaxLength)
+                .IsRequired();
 
             builder.Property(d => d.Password)
                 .HasColumnName("password")
-                .HasMaxLength(UsersPasswordHelper.MaxLength);
+                .HasMaxLength(UsersPasswordHelper.MaxLength)
+                .IsRequired();
 
             builder.Property(d => d.CreatedAt).HasColumnName("createdAt");
             builder.Property(d => d.ExternalId)
                 .HasColumnName("externalId")
                 .IsRequired();
 
-            builder.Property(d => d.Progress).HasColumnName("progress");
-            builder.Property(d => d.Comment).HasColumnName("comment");
-            builder.Property(d => d.AuthorId).HasColumnName("authorId");
-            builder.Property(d => d.DistributorId).HasColumnName("distributorId");
+            builder.Property(d => d.Progress)
+                .HasColumnName("progress")
+                .IsRequired();
+
+            builder.Property(d => d.Comment)
+                .HasColumnName("comment");
+
+            builder.Property(d => d.AuthorId)
+                .HasColumnName("authorId")
+                .IsRequired();
+
+            builder.Property(d => d.DistributorId)
+                .HasColumnName("distributorId")
+                .IsRequired();
 
             builder.HasOne(d => d.Distributor).WithMany().HasForeignKey(d => d.DistributorId);
         }
