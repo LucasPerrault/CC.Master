9609939dc31d24db4d57c59593fa8a6c12d78c34
versinia <angelin.versini@gmail.com>
2021-04-08 12:39:11 +0200

    FIX remote store configuration

--- back/Instances/Instances.Web/InstancesConfigurer.cs
+++ back/Instances/Instances.Web/InstancesConfigurer.cs
@@ -14,13 +14,22 @@ using Lucca.Core.Api.Abstractions;
 using Lucca.Core.Api.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Remote.Infra.Extensions;
+using System;
 
 namespace Instances.Web
 {
     public static class InstancesConfigurer
     {
+        public class InstancesStoreConfiguration
+        {
+            public Uri Host { get; set; }
+            public string Endpoint { get; set; }
+            public Guid Token { get; set; }
+        }
+
         public class InstancesConfiguration
         {
+            public InstancesStoreConfiguration Store { get; set; }
             public IdentityAuthenticationConfig Identity { get; set; }
             public CcDataConfiguration CcData { get; set; }
             public WsAuthConfiguration WsAuth { get; set; }
@@ -32,8 +41,6 @@ namespace Instances.Web
             services.AddSingleton(configuration.CcData);
             services.AddSingleton<IUsersPasswordHelper, UsersPasswordHelper>();
             services.AddSingleton<SqlScriptPicker>();
-
-            services.AddScoped<IInstancesStore, InstancesRemoteStore>();
 
             services.AddScoped<InstancesDuplicator>();
 
@@ -48,6 +55,13 @@ namespace Instances.Web
             services.AddScoped<IDemoUsersPasswordResetService, DemoUsersPasswordResetService>();
 
             services.AddScoped<IUsersPasswordResetService, UsersPasswordResetService>();
+
+            services.AddHttpClient<IInstancesStore, InstancesRemoteStore>(client =>
+            {
+                client.WithUserAgent(nameof(InstancesRemoteStore))
+                    .WithBaseAddress(new Uri(configuration.Store.Host, configuration.Store.Endpoint))
+                    .WithAuthScheme("CloudControl").AuthenticateAsApplication(configuration.Store.Token);
+            });
 
             services.AddHttpClient<WsAuthRemoteService>(client =>
             {
