7e0960b5e3bb2f6bf78886c6aa15c5766d3ce1a8
versinia <angelin.versini@gmail.com>
2021-04-12 12:18:15 +0200

    ADD hangfire controller

---
+++ back/CloudControl.Web/Controllers/HangfireController.cs
@@ -0,0 +1,25 @@
+﻿using Authentication.Web.Attributes;
+using Instances.Application.Demos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CloudControl.Web.Controllers
+{
+    [ApiController, Route("/api/hangfire")]
+    public class HangfireController
+    {
+        private readonly InactiveDemosCleaner _cleaner;
+
+        public HangfireController(InactiveDemosCleaner cleaner)
+        {
+            _cleaner = cleaner;
+        }
+
+        [HangfireAuthorize]
+        [HttpPost("cleanup-demos")]
+        public async Task CleanupDemosAsync()
+        {
+            await _cleaner.CleanAsync();
+        }
+    }
+}
