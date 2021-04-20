c66521606e33da8eb90b93ba68a23bf7751c4252
Perrault <lucas.perrault@hotmail.com>
2021-04-20 16:22:43 +0200

    add nav.tabs token import forgot

--- front/cc-master/src/app/common/navigation/index.ts
+++ front/cc-master/src/app/common/navigation/index.ts
@@ -2,5 +2,7 @@ export * from './constants/navigation-path.enum';
 export * from './constants/navigation-tabs.const';
 export * from './models/navigation-tab.interface';
 export { NavigationModule } from './navigation.module';
+export * from './navigation-tabs.token';
 export * from './services/navigation-alert.service';
 export * from './services/navigation-tabs.service';
+
