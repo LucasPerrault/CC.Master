ac9de30a50b3a369f9f610e1be50d1366fcc54fe
versinia <angelin.versini@gmail.com>
2021-04-30 18:12:38 +0200

    DEL useless method with undesired return type

--- back/Instances/Instances.Domain/Demos/Filtering/DemosRightsFilter.cs
+++ back/Instances/Instances.Domain/Demos/Filtering/DemosRightsFilter.cs
@@ -3,7 +3,6 @@ using Lucca.Core.Rights.Abstractions;
 using Rights.Domain;
 using Rights.Domain.Abstractions;
 using System;
-using System.Linq.Expressions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -32,25 +31,6 @@ namespace Instances.Domain.Demos.Filtering
                     };
                 case CloudControlApiKeyClaimsPrincipal _:
                     return DemoAccess.All;
-                default:
-                    throw new ApplicationException("Unhandled ClaimsPrincipal type");
-            };
-        }
-
-        public async Task<Expression<Func<DemoDuplication, bool>>> GetDuplicationReadFilterAsync(ClaimsPrincipal principal)
-        {
-            switch(principal)
-            {
-                case CloudControlUserClaimsPrincipal userPrincipal:
-                    var currentUserScope = await _rightsService.GetUserOperationHighestScopeAsync(Operation.Demo);
-                    return currentUserScope switch
-                    {
-                        Scope.AllDepartments => _ => true,
-                        Scope.DepartmentOnly => d => d.InstanceDuplication.Distributor.Code == userPrincipal.User.DepartmentCode,
-                        _ => throw new ApplicationException($"Unhandled scope : {currentUserScope}")
-                    };
-                case CloudControlApiKeyClaimsPrincipal apiKey:
-                    return _ => true;
                 default:
                     throw new ApplicationException("Unhandled ClaimsPrincipal type");
             };
