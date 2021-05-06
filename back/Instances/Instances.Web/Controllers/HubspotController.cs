86bfe5b6ab5454f83dc6d08469e2f5424aa18b21
versinia <angelin.versini@gmail.com>
2021-05-06 15:44:08 +0200

    REF split hubspot controller

--- back/Instances/Instances.Web/Controllers/HubspotController.cs
+++ back/Instances/Instances.Web/Controllers/HubspotController.cs
@@ -1,7 +1,5 @@
 using Instances.Application.Demos.Duplication;
 using Instances.Web.Controllers.Dtos;
-using IpFilter.Domain;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,7 +8,7 @@ using System.Threading.Tasks;
 namespace Instances.Web.Controllers
 {
 
-    [ApiController, Route("/api/hubspot")]
+    [ApiController, Route("/api/hubspot/duplications")]
     public class HubspotController
     {
         private readonly HubspotDemoDuplicator _demoDuplicator;
@@ -22,18 +20,7 @@ namespace Instances.Web.Controllers
             _duplicationCompleter = duplicationCompleter;
         }
 
-        public class HubspotDemoDuplicationRequest
-        {
-            public int VId { get; set; }
-        }
-
-        public class HubspotDemoDuplicationQuery
-        {
-            public int SuccessWorkflowId { get; set; }
-            public int FailWorkflowId { get; set; }
-        }
-
-        [HttpPost("duplications/{instanceDuplicationId}/notify")]
+        [HttpPost("{instanceDuplicationId:guid}/notify")]
         public async Task<ActionResult> NotifyDuplicationEndAsync
         (
             [FromRoute]Guid instanceDuplicationId,
@@ -42,33 +29,6 @@ namespace Instances.Web.Controllers
         {
             await _duplicationCompleter.MarkDuplicationAsCompletedAsync(instanceDuplicationId, payload.Success);
             await _demoDuplicator.MarkAsEndedAsync(instanceDuplicationId, payload.Success);
-
-            return new StatusCodeResult(StatusCodes.Status202Accepted);
-        }
-
-        // Hubspot needs anonymous access to this route as it cannot authenticate its calls.
-        // Ip ranges used by hubspot are inconsistent with their documentation.
-        //
-        // We have no choice but open this route to any call,
-        // knowing that an attacker would need to know valid vIds of hubspot contacts
-        // in order to create demos on this route.
-        //
-        // They would also need to know valid workflow ids for emails to reach contacts.
-        [AllowAnonymous]
-        [AllowAllIps]
-        [HttpPost("duplication-request")]
-        public async Task<ActionResult> RequestDuplicationAsync
-        (
-            [FromQuery]HubspotDemoDuplicationQuery duplicationQuery,
-            [FromBody]HubspotDemoDuplicationRequest request
-        )
-        {
-            await _demoDuplicator.DuplicateMasterForHubspotAsync(new HubspotDemoDuplication
-            {
-                VId = request.VId,
-                SuccessWorkflowId = duplicationQuery.SuccessWorkflowId,
-                FailureWorkflowId = duplicationQuery.FailWorkflowId
-            });
 
             return new StatusCodeResult(StatusCodes.Status202Accepted);
         }
