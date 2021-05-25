983ae2071bce5046352b840f69218a6d4fb6d941
pierre bochu <40466697+boboch86@users.noreply.github.com>
2021-05-25 10:03:01 +0200

    populate database with business unit

--- back/Billing/Billing.Products/Billing.Products.Infra/Migrations/20210524131222_CreateBusinessUnitsTable.cs
+++ back/Billing/Billing.Products/Billing.Products.Infra/Migrations/20210524131222_CreateBusinessUnitsTable.cs
@@ -33,6 +33,50 @@ namespace Billing.Products.Infra.Migrations
                 table: "Solutions",
                 column: "BusinessUnitId");
 
+            migrationBuilder.Sql(@"
+                INSERT INTO [CLOUDCONTROL].[billing].[BusinessUnit]
+                VALUES
+                ('GTA'),
+                ('Finance'),
+                ('Talent'),
+                ('Pay'),
+                ('Core'),
+                ('Employee Engagement'),
+                ('Others')
+                ");
+
+            migrationBuilder.Sql(@"
+                UPDATE [CLOUDCONTROL].[billing].[Solutions]
+                SET BusinessUnitId = (
+                CASE [Solutions].name 
+                       WHEN 'Figgo' THEN 1
+                       WHEN 'Cleemy' THEN 2
+                       WHEN 'Pagga' THEN 4
+                       WHEN 'Pay Manager' THEN 7
+                       WHEN 'Poplee Core RH' THEN 5
+                       WHEN 'Portail' THEN 7
+                       WHEN 'Spensy' THEN 7
+                       WHEN 'Urba' THEN 7
+                       WHEN 'Timesheet (old)' THEN 1
+                       WHEN 'Ucal' THEN 7
+                       WHEN 'Planner' THEN 7
+                       WHEN 'Calendar' THEN 7
+                       WHEN 'Autres' THEN 7
+                       WHEN 'Admin' THEN 7
+                       WHEN 'Project Manager' THEN 7
+                       WHEN 'Timmi Timesheet' THEN 1
+                       WHEN 'Anytime' THEN 2
+                       WHEN 'Budget Insight' THEN 2
+                       WHEN 'Timmi Project' THEN 1
+                       WHEN 'Poplee REM' THEN 4
+                       WHEN 'Dématérialisation' THEN 2
+                       WHEN 'Synchronisation Figgo/GXP' THEN 1
+                       WHEN 'Poplee Entretiens & Objectifs' THEN 3
+                       WHEN 'Signature Electronique' THEN 7 --maquant
+                END
+                )
+                ");
+
             migrationBuilder.AddForeignKey(
                 name: "FK_Solutions_BusinessUnit_BusinessUnitId",
                 schema: "billing",
