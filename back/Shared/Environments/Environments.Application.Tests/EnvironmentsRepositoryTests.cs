2646fd57af19c7275f2db6756171a380e3d6da6a
versinia <angelin.versini@gmail.com>
2021-06-02 14:47:51 +0200

    ADD env repo tests

---
+++ back/Shared/Environments/Environments.Application.Tests/EnvironmentsRepositoryTests.cs
@@ -0,0 +1,155 @@
+using Authentication.Domain;
+using Distributors.Domain.Models;
+using Environments.Domain;
+using Environments.Domain.Storage;
+using Moq;
+using Rights.Domain;
+using Rights.Domain.Abstractions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Users.Domain;
+using Xunit;
+
+namespace Environments.Application.Tests
+{
+    public class EnvironmentsRepositoryTests
+    {
+        private readonly Mock<IEnvironmentsStore> _envStoreMock = new Mock<IEnvironmentsStore>();
+        private readonly Mock<IRightsService> _rightServiceMock = new Mock<IRightsService>();
+
+        public EnvironmentsRepositoryTests()
+        {
+            _rightServiceMock
+                .Setup(s => s.GetScopedPermissionsAsync(Operation.ReadEnvironments))
+                .ReturnsAsync(new List<ScopedPermission>());
+        }
+
+        [Fact]
+        public async Task ShouldReturnNothingAccess()
+        {
+            var environment = new Environment
+            {
+                Subdomain = "aperture-science",
+                ActiveAccesses = new List<EnvironmentSharedAccess>()
+            };
+
+            _envStoreMock
+                .Setup(e => e.GetAsync(It.IsAny<List<EnvironmentAccessRight>>(), It.IsAny<EnvironmentFilter>()))
+                .ReturnsAsync(new List<Environment> { environment });
+
+            var repository = NewRepository();
+            var accesses = await repository.GetAccessesAsync("aperture-science");
+
+            Assert.Equal(accesses, new HashSet<DistributorWithAccess>());
+        }
+
+        [Fact]
+        public async Task ShouldReturnSimpleAccess()
+        {
+            var environment = new Environment
+            {
+                Subdomain = "aperture-science",
+                ActiveAccesses = new List<EnvironmentSharedAccess>
+                {
+                    new EnvironmentSharedAccess
+                    {
+                        Consumer = new Distributor { Code = "APERTURE" },
+                        Access = new EnvironmentAccess {Type = EnvironmentAccessTypeEnum.Contract }
+                    }
+                }
+            };
+
+            _envStoreMock
+                .Setup(e => e.GetAsync(It.IsAny<List<EnvironmentAccessRight>>(), It.IsAny<EnvironmentFilter>()))
+                .ReturnsAsync(new List<Environment> { environment });
+
+            var repository = NewRepository();
+            var accesses = await repository.GetAccessesAsync("aperture-science");
+
+            Assert.Equal(accesses, new HashSet<DistributorWithAccess>
+            {
+                new DistributorWithAccess("APERTURE", new List<EnvironmentAccessTypeEnum> { EnvironmentAccessTypeEnum.Contract })
+            });
+        }
+
+        [Fact]
+        public async Task ShouldReturnAccessWithTwoTypes()
+        {
+            var environment = new Environment
+            {
+                Subdomain = "aperture-science",
+                ActiveAccesses = new List<EnvironmentSharedAccess>
+                {
+                    new EnvironmentSharedAccess
+                    {
+                        Consumer = new Distributor { Code = "APERTURE" },
+                        Access = new EnvironmentAccess {Type = EnvironmentAccessTypeEnum.Contract }
+                    },
+                    new EnvironmentSharedAccess
+                    {
+                        Consumer = new Distributor { Code = "APERTURE" },
+                        Access = new EnvironmentAccess {Type = EnvironmentAccessTypeEnum.Manual }
+                    },
+                }
+            };
+
+            _envStoreMock
+                .Setup(e => e.GetAsync(It.IsAny<List<EnvironmentAccessRight>>(), It.IsAny<EnvironmentFilter>()))
+                .ReturnsAsync(new List<Environment> { environment });
+
+            var repository = NewRepository();
+            var accesses = await repository.GetAccessesAsync("aperture-science");
+
+            Assert.Equal(accesses, new HashSet<DistributorWithAccess>
+            {
+                new DistributorWithAccess("APERTURE", new List<EnvironmentAccessTypeEnum> { EnvironmentAccessTypeEnum.Contract, EnvironmentAccessTypeEnum.Manual })
+            });
+        }
+
+        [Fact]
+        public async Task ShouldReturnTwoAccesses()
+        {
+            var environment = new Environment
+            {
+                Subdomain = "aperture-science",
+                ActiveAccesses = new List<EnvironmentSharedAccess>
+                {
+                    new EnvironmentSharedAccess
+                    {
+                        Consumer = new Distributor { Code = "APERTURE" },
+                        Access = new EnvironmentAccess {Type = EnvironmentAccessTypeEnum.Contract }
+                    },
+                    new EnvironmentSharedAccess
+                    {
+                        Consumer = new Distributor { Code = "MESA" },
+                        Access = new EnvironmentAccess {Type = EnvironmentAccessTypeEnum.Contract }
+                    },
+                }
+            };
+
+            _envStoreMock
+                .Setup(e => e.GetAsync(It.IsAny<List<EnvironmentAccessRight>>(), It.IsAny<EnvironmentFilter>()))
+                .ReturnsAsync(new List<Environment> { environment });
+
+            var repository = NewRepository();
+            var accesses = await repository.GetAccessesAsync("aperture-science");
+
+            Assert.Equal(accesses, new HashSet<DistributorWithAccess>
+            {
+                new DistributorWithAccess("APERTURE", new List<EnvironmentAccessTypeEnum> { EnvironmentAccessTypeEnum.Contract }),
+                new DistributorWithAccess("MESA", new List<EnvironmentAccessTypeEnum> { EnvironmentAccessTypeEnum.Contract }),
+            });
+        }
+
+        private EnvironmentsRepository NewRepository()
+        {
+            var principal = new CloudControlUserClaimsPrincipal(new Principal
+            {
+                UserId = 1,
+                User = new User { FirstName = "Gla", LastName = "DOS" }
+            });
+
+            return new EnvironmentsRepository(_envStoreMock.Object, principal, new EnvironmentRightsFilter(_rightServiceMock.Object));
+        }
+    }
+}
--- back/Shared/Environments/Environments.Domain/Storage/EnvironmentFilter.cs
+++ back/Shared/Environments/Environments.Domain/Storage/EnvironmentFilter.cs
@@ -1,11 +1,22 @@
-﻿using Tools;
+﻿using System.Collections.Generic;
+using Tools;
 
 namespace Environments.Domain.Storage
 {
-    public class EnvironmentFilter
+    public class EnvironmentFilter : ValueObject
     {
         public CompareString Subdomain { get; set; } = CompareString.Bypass;
         public CompareBoolean IsActive { get; set; } = CompareBoolean.Bypass;
         public string Search { get; set; }
+
+        protected override IEnumerable<object> EqualityComponents
+        {
+            get
+            {
+                yield return Subdomain;
+                yield return IsActive;
+                yield return Search;
+            }
+        }
     }
 }
