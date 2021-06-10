123e8645bd2f6f9a5b68ac222ea61c6531a9c7a5
versinia <angelin.versini@gmail.com>
2021-06-10 15:54:11 +0200

    ADD configurations and migrations

---
+++ back/Instances/Instances.Infra/Migrations/20210610135136_CreateCodeSources.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Instances.Infra.Migrations
+{
+    public partial class CreateCodeSources : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                ALTER SCHEMA instances TRANSFER dbo.CodeSources;
+            ");
+            migrationBuilder.Sql($@"
+                ALTER SCHEMA instances TRANSFER dbo.CodeSourceConfigs;
+            ");
+            migrationBuilder.Sql($@"
+                ALTER SCHEMA instances TRANSFER dbo.CodeSourcesProductionVersions;
+            ");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeSourceConfigs_CodeSourceId",
+                schema: "instances",
+                table: "CodeSourceConfigs",
+                column: "CodeSourceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeSourcesProductionVersions_CodeSourceId",
+                schema: "instances",
+                table: "CodeSourcesProductionVersions",
+                column: "CodeSourceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
---
+++ back/Instances/Instances.Infra/Storage/Configurations/CodeSourcesConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Instances.Domain.CodeSources;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Instances.Infra.Storage.Configurations
+{
+    public class CodeSourcesConfiguration : IEntityTypeConfiguration<CodeSource>
+    {
+        public void Configure(EntityTypeBuilder<CodeSource> builder)
+        {
+            builder.ToTable("CodeSources");
+            builder.Property(cs => cs.Code).HasColumnName("Code");
+            builder.Property(cs => cs.Name).HasColumnName("Name");
+            builder.Property(cs => cs.JenkinsProjectName).HasColumnName("JenkinsProjectName");
+            builder.Property(cs => cs.Type).HasColumnName("Type");
+            builder.Property(cs => cs.GithubRepo).HasColumnName("GithubRepo");
+            builder.Property(cs => cs.Lifecycle).HasColumnName("Lifecycle");
+
+            builder.HasMany(cs => cs.ProductionVersions).WithOne().HasForeignKey(pv => pv.CodeSourceId);
+        }
+    }
+
+    public class CodeSourceConfigsConfiguration : IEntityTypeConfiguration<CodeSourceConfig>
+    {
+        public void Configure(EntityTypeBuilder<CodeSourceConfig> builder)
+        {
+            builder.ToTable("CodeSourceConfigs");
+            builder.HasKey("CodeSourceId");
+            builder.Property(c => c.AppPath).HasColumnName("AppPath");
+            builder.Property(c => c.Subdomain).HasColumnName("Subdomain");
+            builder.Property(c => c.IisServerPath).HasColumnName("IisServerPath");
+            builder.Property(c => c.IsPrivate).HasColumnName("IsPrivate");
+            builder.HasOne(c => c.CodeSource).WithOne(cs => cs.Config).HasForeignKey<CodeSourceConfig>(c => c.CodeSourceId);
+
+        }
+    }
+
+    public class CodeSourceProductionVersionsConfiguration : IEntityTypeConfiguration<CodeSourceProductionVersion>
+    {
+        public void Configure(EntityTypeBuilder<CodeSourceProductionVersion> builder)
+        {
+            builder.ToTable("CodeSourcesProductionVersions");
+            builder.HasKey("Id");
+            builder.Property(c => c.CodeSourceId).HasColumnName("CodeSourceId");
+            builder.Property(c => c.BranchName).HasColumnName("BranchName");
+            builder.Property(c => c.JenkinsBuildNumber).HasColumnName("JenkinsBuildNumber");
+            builder.Property(c => c.CommitHash).HasColumnName("CommitHash");
+            builder.Property(c => c.Date).HasColumnName("Date");
+        }
+    }
+}
