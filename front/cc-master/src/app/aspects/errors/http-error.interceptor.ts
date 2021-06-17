c35e356f35814458c6033594cd09b88cc032bb46
Perrault <lucas.perrault@hotmail.com>
2021-06-17 14:15:37 +0200

    mod get http message method

--- front/cc-master/src/app/aspects/errors/http-error.interceptor.ts
+++ front/cc-master/src/app/aspects/errors/http-error.interceptor.ts
@@ -36,15 +36,16 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     const message = this.getHttpErrorMessage(httpErrorResponse);
     const status = httpErrorResponse.status;
     const statusText = httpErrorResponse.statusText;
-    return `${status} ${statusText} ${message}`;
+
+    return !!message
+      ? `${status} ${statusText} : ${message}`
+      : `${status} ${statusText}`;
   }
 
   private getHttpErrorMessage(httpErrorResponse: HttpErrorResponse): string {
-    const message = httpErrorResponse.error?.Message
+    return httpErrorResponse.error?.Message
       || httpErrorResponse.error?.message
       || httpErrorResponse.error?.Detail
       || httpErrorResponse.error?.detail;
-
-    return !!message ? `: ${ message }` : '';
   }
 }
