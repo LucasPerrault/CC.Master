1f48f4726e11c1522ac9b262124b92487148917d
versinia <angelin.versini@gmail.com>
2021-07-07 15:43:21 +0200

    MOD solution shares

--- back/Billing/Billing.Products/Billing.Products.Infra/Migrations/20210609090532_AlterProductsAddShare.cs
+++ back/Billing/Billing.Products/Billing.Products.Infra/Migrations/20210609090532_AlterProductsAddShare.cs
@@ -17,31 +17,31 @@ namespace Billing.Products.Infra.Migrations
                 UPDATE [CLOUDCONTROL].[billing].[Solutions]
                 SET defaultBreakdownShare = (
                 CASE [Solutions].name
-                       WHEN 'Figgo' THEN 100
-                       WHEN 'Cleemy' THEN 100
-                       WHEN 'Pagga' THEN 100
-                       WHEN 'Pay Manager' THEN 1
-                       WHEN 'Poplee Core RH' THEN 100
-                       WHEN 'Portail' THEN 1
-                       WHEN 'Spensy' THEN 1
-                       WHEN 'Urba' THEN 1
-                       WHEN 'Timesheet (old)' THEN 1
-                       WHEN 'Ucal' THEN 1
-                       WHEN 'Planner' THEN 1
-                       WHEN 'Calendar' THEN 1
-                       WHEN 'Autres' THEN 1
-                       WHEN 'Admin' THEN 1
-                       WHEN 'Project Manager' THEN 1
-                       WHEN 'Timmi Timesheet' THEN 100
-                       WHEN 'Anytime' THEN 10
-                       WHEN 'Budget Insight' THEN 10
-                       WHEN 'Timmi Project' THEN 100
-                       WHEN 'Poplee REM' THEN 100
-                       WHEN 'Dématérialisation' THEN 10
-                       WHEN 'Synchronisation Figgo/GXP' THEN 10
-                       WHEN 'Poplee Entretiens & Objectifs' THEN 100
-                       WHEN 'Signature Electronique' THEN 10
-                       WHEN 'Bloom At Work' THEN 100
+                        WHEN 'Figgo' THEN 38
+                        WHEN 'Cleemy' THEN 26
+                        WHEN 'Pagga' THEN 7
+                        WHEN 'Pay Manager' THEN 1
+                        WHEN 'Poplee Core RH' THEN 36
+                        WHEN 'Portail' THEN 1
+                        WHEN 'Spensy' THEN 1
+                        WHEN 'Urba' THEN 1
+                        WHEN 'Timesheet (old)' THEN 1
+                        WHEN 'Ucal' THEN 1
+                        WHEN 'Planner' THEN 1
+                        WHEN 'Calendar' THEN 1
+                        WHEN 'Autres' THEN 1
+                        WHEN 'Admin' THEN 1
+                        WHEN 'Project Manager' THEN 1
+                        WHEN 'Timmi Timesheet' THEN 38
+                        WHEN 'Anytime' THEN 5
+                        WHEN 'Budget Insight' THEN 5
+                        WHEN 'Timmi Project' THEN 20
+                        WHEN 'Poplee REM' THEN 19
+                        WHEN 'Dématérialisation' THEN 0
+                        WHEN 'Synchronisation Figgo/GXP' THEN 5
+                        WHEN 'Poplee Entretiens & Objectifs' THEN 39
+                        WHEN 'Signature Electronique' THEN 0
+                        WHEN 'Bloom At Work' THEN 20
                 END)");
 
         }
