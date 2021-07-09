1cb5dc4e7cc7403fd6cc156ee72ab5db3e9b9a47
Stéphane Seguin <sseguin@lucca.fr>
2021-07-09 18:05:21 +0200

    Pre-fill JenkinsProjectUrl when running migration

--- back/Instances/Instances.Infra/Migrations/20210705100002_JenkinsUrlCodeSource.cs
+++ back/Instances/Instances.Infra/Migrations/20210705100002_JenkinsUrlCodeSource.cs
@@ -8,7 +8,19 @@ namespace Instances.Infra.Migrations
         {
             migrationBuilder.Sql(@"
                 ALTER table [instances].[CodeSources]
-                ADD JenkinsProjectUrl nvarchar
+                ADD JenkinsProjectUrl nvarchar(max)
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE [instances].[CodeSources]
+                   SET JenkinsProjectUrl = CONCAT('http://jenkins2.lucca.local:8080/job/',
+                      CASE Type
+                         WHEN 0 THEN CONCAT('NoMonolith', '/job/', jenkinsProjectName)
+                         WHEN 1 THEN CONCAT('webservices', '/job/', jenkinsProjectName)
+                         WHEN 2 THEN 'CI.Pipeline.Multibranch'
+                         WHEN 3 THEN CONCAT('webservices-legacy', '/job/', jenkinsProjectName)
+                         ELSE 'webservices'
+                      END)
             ");
         }
 
