b1942b61df5e6712fa79aff1ce1a18096f69af68
Guillaume Mouron <gmouron@lucca.fr>
2021-07-09 12:27:35 +0200

    [ADD] Tests DNS

---
+++ back/Instances/Instances.Infra.Tests/Dns/OvhDnsServiceTest.cs
@@ -0,0 +1,272 @@
+using FluentAssertions;
+using Instances.Infra.Dns;
+using Instances.Infra.Shared;
+using Moq;
+using Moq.Protected;
+using Ovh.Api;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Instances.Infra.Tests.Dns
+{
+    public class OvhDnsServiceTest
+    {
+        private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
+        private readonly Client _ovhApiClient;
+
+        public OvhDnsServiceTest()
+        {
+            _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            _ovhApiClient = new Client("ovh-eu", "app-key", "app-secret", "consumer-key", null, ',', new HttpClient(_httpMessageHandlerMock.Object));
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),//req => req.RequestUri.PathAndQuery.Contains("/auth/time")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent($"{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}")
+               });
+            //
+        }
+
+        #region AddNewCnameAsync
+        [Fact]
+        public async Task AddNewCnameAsync_ShouldPostToTheCorrectZoneAndRefreshAtTheEnd()
+        {
+            var isCreationCalled = false;
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Post &&
+                  req.RequestUri.PathAndQuery.Contains("/record")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               }).Callback(() => isCreationCalled = true);
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Post &&
+                  req.RequestUri.PathAndQuery.Contains("/refresh")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               }).Callback(() => Assert.True(isCreationCalled));
+
+            var ovhDnsService = new OvhDnsService(_ovhApiClient);
+            var dnsEntryCreation = new DnsEntryCreation
+            {
+                Cluster = "demo",
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            Func<Task> act = async () => await ovhDnsService.AddNewCnameAsync(dnsEntryCreation);
+
+            await act.Should().NotThrowAsync();
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Once(),
+                   ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Post &&
+                        req.RequestUri.PathAndQuery.Contains($"/domain/zone/{dnsEntryCreation.DnsZone}/record")),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Once(),
+                   ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Post &&
+                        req.RequestUri.PathAndQuery.Contains($"/domain/zone/{dnsEntryCreation.DnsZone}/refresh")),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+        }
+
+        #endregion
+
+        #region DeleteCnameAsync
+        [Fact]
+        public async Task DeleteCnameAsync_ShouldNotCallDeleteIfThereIsNoExistingRecord()
+        {
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Get &&
+                  req.RequestUri.PathAndQuery.Contains("/record")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("[]")
+               });
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Delete),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               });
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Post),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               });
+
+            var ovhDnsService = new OvhDnsService(_ovhApiClient);
+            var dnsEntryDeletion = new DnsEntryDeletion
+            {
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            Func<Task> act = async () => await ovhDnsService.DeleteCnameAsync(dnsEntryDeletion);
+
+            await act.Should().NotThrowAsync();
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Never(),
+                   ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Delete),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Never(),
+                   ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Post),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+        }
+
+        [Fact]
+        public async Task DeleteCnameAsync_ShouldDeleteToTheCorrectZoneAndRefreshAtTheEnd()
+        {
+            var isGetRecordIdCalled = false;
+            var isDeletionCalled = false;
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Get &&
+                  req.RequestUri.PathAndQuery.Contains("/record")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("[1]")
+               }).Callback(() => isGetRecordIdCalled = true);
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Delete &&
+                  req.RequestUri.PathAndQuery.Contains("/record")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               }).Callback(() => {
+                   Assert.True(isGetRecordIdCalled, "La récupération du recordId n'a pas été appelée avant la suppression");
+                   isDeletionCalled = true;
+                });
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(req =>
+                  req.Method == HttpMethod.Post &&
+                  req.RequestUri.PathAndQuery.Contains("/refresh")),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(new HttpResponseMessage
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent("")
+               }).Callback(() => Assert.True(isDeletionCalled, "La suppression n'a pas été appelée avant le refresh"));
+
+            var ovhDnsService = new OvhDnsService(_ovhApiClient);
+            var dnsEntryDeletion = new DnsEntryDeletion
+            {
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            Func<Task> act = async () => await ovhDnsService.DeleteCnameAsync(dnsEntryDeletion);
+
+            await act.Should().NotThrowAsync();
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Once(),
+                   ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Delete &&
+                        req.RequestUri.PathAndQuery.Contains($"/domain/zone/{dnsEntryDeletion.DnsZone}/record")),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+
+            _httpMessageHandlerMock
+                .Protected()
+                .Verify(
+                   "SendAsync",
+                   Times.Once(),
+                   ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Post &&
+                        req.RequestUri.PathAndQuery.Contains($"/domain/zone/{dnsEntryDeletion.DnsZone}/refresh")),
+                   ItExpr.IsAny<CancellationToken>()
+               );
+        }
+
+        #endregion
+
+    }
+}
---
+++ back/Instances/Instances.Infra.Tests/Dns/WinDnsServiceTest.cs
@@ -0,0 +1,122 @@
+using Instances.Infra.Dns;
+using Instances.Infra.Shared;
+using Instances.Infra.Windows;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Instances.Infra.Tests.Dns
+{
+    public class WinDnsServiceTest
+    {
+        private readonly Mock<IWmiWrapper> _wmiWrapperMock;
+
+        public WinDnsServiceTest()
+        {
+            _wmiWrapperMock = new Mock<IWmiWrapper>();
+        }
+
+        #region AddNewCname
+        [Fact]
+        public void AddNewCname_ShouldCallWmiWithTheCorrectData()
+        {
+            var internalDnsConfiguration = new WinDnsConfiguration { Server = "my-dns-server" };
+            _wmiWrapperMock.Setup(w => w.InvokeClassMethod(It.IsAny<WmiSessionWrapper>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Dictionary<string, object>>()));
+
+            var winDnsService = new WinDnsService(internalDnsConfiguration, _wmiWrapperMock.Object);
+            var dnsEntryCreation = new DnsEntryCreation
+            {
+                Cluster = "demo",
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            winDnsService.AddNewCname(dnsEntryCreation);
+
+            _wmiWrapperMock.Verify(w => w.InvokeClassMethod(
+                It.IsAny<WmiSessionWrapper>(),
+                It.Is<string>(className => className == WmiConstants.ClassCNameType),
+                It.Is<string>(method => method == WmiConstants.MethodCreateInstanceFromPropertyData),
+                It.Is<Dictionary<string, object>>(d =>
+                    d.ContainsKey(WmiConstants.PropertyCNameTypeContainerName)
+                    && (string)d[WmiConstants.PropertyCNameTypeContainerName] == dnsEntryCreation.DnsZone
+                    && d.ContainsKey(WmiConstants.PropertyCNameTypeDnsServerName)
+                    && (string)d[WmiConstants.PropertyCNameTypeDnsServerName] == internalDnsConfiguration.Server
+                    && d.ContainsKey(WmiConstants.PropertyCNameTypeOwnerName)
+                     && (string)d[WmiConstants.PropertyCNameTypeOwnerName] == $"{dnsEntryCreation.Subdomain}.{dnsEntryCreation.DnsZone}"
+                    && d.ContainsKey(WmiConstants.PropertyCNameTypePrimaryName)
+                     && (string)d[WmiConstants.PropertyCNameTypePrimaryName] == $"rbx-{ClusterNameConvertor.GetShortName(dnsEntryCreation.Cluster)}-haproxy.lucca.local."
+                )), Times.Once
+            );
+        }
+
+        [Fact]
+        public void AddNewCname_ShouldConnectToTheCorrectPath()
+        {
+            var internalDnsConfiguration = new WinDnsConfiguration { Server = "my-dns-server" };
+            _wmiWrapperMock.Setup(w => w.CreateSession(It.IsAny<string>()));
+
+            var winDnsService = new WinDnsService(internalDnsConfiguration, _wmiWrapperMock.Object);
+            var dnsEntryCreation = new DnsEntryCreation
+            {
+                Cluster = "demo",
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            winDnsService.AddNewCname(dnsEntryCreation);
+
+            _wmiWrapperMock.Verify(w => w.CreateSession(It.Is<string>(sessionPath => sessionPath == @$"\\{internalDnsConfiguration.Server}\root\microsoftdns")), Times.Once);
+        }
+
+        #endregion
+
+        #region DeleteCname
+        [Fact]
+        public void DeleteCname_ShouldQueryUsingTheCorrectData()
+        {
+            var internalDnsConfiguration = new WinDnsConfiguration { Server = "my-dns-server" };
+            _wmiWrapperMock.Setup(w => w.QueryAndDeleteObjects(It.IsAny<IWmiSessionWrapper>(), It.IsAny<string>()));
+
+            var winDnsService = new WinDnsService(internalDnsConfiguration, _wmiWrapperMock.Object);
+            var dnsEntryDeletion = new DnsEntryDeletion
+            {
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            winDnsService.DeleteCname(dnsEntryDeletion);
+
+
+            _wmiWrapperMock.Verify(w => w.QueryAndDeleteObjects(It.IsAny<IWmiSessionWrapper>(),
+                It.Is<string>(q => q.Contains(WmiConstants.ClassCNameType)
+                    && q.Contains(WmiConstants.PropertyCNameTypeOwnerName)
+                    && q.Contains($"{dnsEntryDeletion.Subdomain}.{dnsEntryDeletion.DnsZone}"))
+                ), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteCname_ShouldConnectToTheCorrectPath()
+        {
+            var internalDnsConfiguration = new WinDnsConfiguration { Server = "my-dns-server" };
+            _wmiWrapperMock.Setup(w => w.CreateSession(It.IsAny<string>()));
+
+            var winDnsService = new WinDnsService(internalDnsConfiguration, _wmiWrapperMock.Object);
+            var dnsEntryDeletion = new DnsEntryDeletion
+            {
+                DnsZone = "my-zone",
+                Subdomain = "des-maux",
+            };
+
+            winDnsService.DeleteCname(dnsEntryDeletion);
+
+            _wmiWrapperMock.Verify(w => w.CreateSession(It.Is<string>(sessionPath => sessionPath == @$"\\{internalDnsConfiguration.Server}\root\microsoftdns")), Times.Once);
+        }
+
+        #endregion
+
+    }
+}
