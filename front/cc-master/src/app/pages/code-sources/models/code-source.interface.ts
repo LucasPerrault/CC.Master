623fe6fb3eb0e161a216be7d84ac2c30e6fb3e19
Guillaume Mouron <gmouron@lucca.fr>
2021-07-11 22:10:35 +0200

    [FIX] Mauvaise capitalisation pour récupérer l'url du job jenkins

--- front/cc-master/src/app/pages/code-sources/models/code-source.interface.ts
+++ front/cc-master/src/app/pages/code-sources/models/code-source.interface.ts
@@ -10,6 +10,6 @@ export interface ICodeSource {
   githubRepo: string;
   lifecycle: LifecycleStep;
   jenkinsProjectName: string;
-  JenkinsProjectUrl: string;
+  jenkinsProjectUrl: string;
   config: ICodeSourceConfig;
 }
