b3ecec93e01636587c20fe31e1188b076db51dd3
Angelin Versini <aversini@lucca.fr>
2021-07-15 10:08:44 +0200

    REF get as little data as possible from database

--- back/Billing/Billing.Cmrr/Billing.Cmrr.Domain/CmrrContract.cs
+++ back/Billing/Billing.Cmrr/Billing.Cmrr.Domain/CmrrContract.cs
@@ -7,7 +7,6 @@ namespace Billing.Cmrr.Domain
         public int Id { get; set; }
         public int ProductId { get; set; }
 
-        public DateTime CreationDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
@@ -18,8 +17,6 @@ namespace Billing.Cmrr.Domain
         public string ClientName { get; set; }
 
         public int DistributorId { get; set; }
-
-        public int EnvironmentId { get; set; }
         public DateTime EnvironmentCreatedAt { get; set; }
 
         public bool IsArchived { get; set; }
--- back/Billing/Billing.Cmrr/Billing.Cmrr.Domain/CmrrCount.cs
+++ back/Billing/Billing.Cmrr/Billing.Cmrr.Domain/CmrrCount.cs
@@ -9,18 +9,8 @@ namespace Billing.Cmrr.Domain
         public int ContractId { get; set; }
         public DateTime CountPeriod { get; set; }
         public BillingStrategy BillingStrategy { get; set; }
-
         public int AccountingNumber { get; set; }
-        public int EntryNumber { get; set; }
-        public string Code { get; set; }
-
-        public int CurrencyId { get; set; }
-        public decimal CurrencyTotal { get; set; }
         public decimal EuroTotal { get; set; }
-        public double LuccaDiscount { get; set; }
-        public double DistributorDiscount { get; set; }
-
-        public DateTime CreatedAt { get; set; }
     }
 
     public enum BillingStrategy
