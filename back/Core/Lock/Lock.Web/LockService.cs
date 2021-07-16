aadc6dbae516a63bfadeaca1eac0cadccfa39acb
Angelin Versini <aversini@lucca.fr>
2021-07-16 09:48:31 +0200

    ADD lock acquirement during duplication
    ensures two duplication with same subdomain can't be concurrently requested

---
+++ back/Core/Lock/Lock.Web/LockService.cs
@@ -0,0 +1,28 @@
+﻿using Lucca.Core.Shared.Domain.Exceptions;
+using Medallion.Threading.SqlServer;
+using System;
+using System.Threading.Tasks;
+
+namespace Lock.Web
+{
+    public class LockService : ILockService
+    {
+        private readonly string _databaseConnectionString;
+
+        public LockService(string databaseConnectionString)
+        {
+            _databaseConnectionString = databaseConnectionString;
+        }
+
+        public async Task<IDisposable> TakeLockAsync(string lockName, TimeSpan timeout)
+        {
+            var distributedLock = new SqlDistributedLock(lockName, _databaseConnectionString);
+            var disposableAcquiredLock = await distributedLock.TryAcquireAsync(timeout);
+            if (disposableAcquiredLock is null)
+            {
+                throw new DomainException(DomainExceptionCode.Conflict, "Resource is busy");
+            }
+            return disposableAcquiredLock;
+        }
+    }
+}
