912455a3b878de443a576db89b67ec4fe459046b
Angelin Versini <aversini@lucca.fr>
2021-07-19 12:19:32 +0200

    MOD extract lazy with reset

---
+++ back/Core/Tools/Tools/LazyWithReset.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Tools
+{
+    public class LazyWithReset<T> where T : class
+    {
+        private readonly Func<T> _init;
+        private T _instance;
+        private readonly object locker = new object();
+
+        public LazyWithReset(Func<T> init)
+        {
+            this._init = init;
+            this._instance = null;
+        }
+
+        private T Value
+        {
+            get
+            {
+                lock (locker)
+                {
+                    return _instance ??= _init();
+                }
+            }
+        }
+
+        public void SafeRun(Action<T> action)
+        {
+            try
+            {
+                action(Value);
+            }
+            catch (Exception)
+            {
+                _instance = null;
+                throw;
+            }
+        }
+    }
+}
