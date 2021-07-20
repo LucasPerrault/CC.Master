5b6edf15a3f23fe761a5f924d1c061a17e7e717a
Angelin Versini <aversini@lucca.fr>
2021-07-20 14:42:13 +0200

    Add permission serialization

---
+++ back/Core/Rights/Rights.Infra.Tests/Extensions/PermissionsExtensionsTests.cs
@@ -0,0 +1,133 @@
+﻿using FluentAssertions;
+using Lucca.Core.Rights.Abstractions;
+using Rights.Infra.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Rights.Infra.Tests.Extensions
+{
+    public class PermissionsExtensionsTests
+    {
+
+        [Theory]
+        [ClassData(typeof(PermissionGenerator))]
+        public void PermissionSerializationShouldWork(Permission permission)
+        {
+            permission.ToBytes().ToPermission().Should().BeEquivalentTo(permission);
+        }
+
+        [Fact]
+        public void PermissionsSerializationShouldWork()
+        {
+            PermissionGenerator.Permissions
+                .ToBytes()
+                .ToPermissions()
+                .Should().BeEquivalentTo(PermissionGenerator.Permissions);
+        }
+
+        [Theory]
+        [ClassData(typeof(ApiKeyPermissionGenerator))]
+        public void ApiKeyPermissionSerializationShouldWork(ApiKeyPermission permission)
+        {
+            permission.ToBytes().ToApiKeyPermission().Should().BeEquivalentTo(permission);
+        }
+
+        [Fact]
+        public void ApiKeyPermissionsSerializationShouldWork()
+        {
+            ApiKeyPermissionGenerator.ApiKeyPermissions
+                .ToBytes()
+                .ToApiKeyPermissions()
+                .Should().BeEquivalentTo(ApiKeyPermissionGenerator.ApiKeyPermissions);
+        }
+    }
+
+    public class PermissionGenerator : IEnumerable<object[]>
+    {
+        public static readonly List<Permission> Permissions = new List<Permission>
+        {
+            new Permission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+            },
+            new Permission
+            {
+                Scope = Scope.DepartmentOnly,
+                OperationId = 3,
+                ExternalEntityId = 25,
+            },
+            new Permission
+            {
+                Scope = Scope.DepartementLevel3,
+                OperationId = 3,
+                ExternalEntityId = 25,
+            },
+            new Permission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+                LegalEntityId = 1,
+            },
+            new Permission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+                SpecificUserId = 1,
+            },
+            new Permission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+                SpecificDepartmentId = 1,
+            },
+            new Permission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+                HasContextualLegalEntityAssociation = true,
+            }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => Permissions.Select(p => new object[] { p }).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class ApiKeyPermissionGenerator : IEnumerable<object[]>
+    {
+        public static readonly List<ApiKeyPermission> ApiKeyPermissions = new List<ApiKeyPermission>
+        {
+            new ApiKeyPermission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+            },
+            new ApiKeyPermission
+            {
+                Scope = Scope.Manager,
+                OperationId = 3,
+                ExternalEntityId = 25,
+            },
+            new ApiKeyPermission
+            {
+                Scope = Scope.AllDepartments,
+                OperationId = 3,
+                ExternalEntityId = 25,
+                LegalEntityId = 1,
+            }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => ApiKeyPermissions.Select(p => new object[] { p }).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- back/Core/Rights/Rights.Infra/Models/Permission.cs
+++ back/Core/Rights/Rights.Infra/Models/Permission.cs
@@ -1,9 +1,13 @@
 using Lucca.Core.Rights.Abstractions;
 using Lucca.Core.Rights.Abstractions.Permissions;
+using MoreLinq.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Rights.Infra.Models
 {
-    internal class Permission : IUserPermission
+    public class Permission : IUserPermission
     {
         public static readonly string ApiFields = $"{nameof(LegalEntityId)},{nameof(Scope)},{nameof(ExternalEntityId)},{nameof(SpecificDepartmentId)},{nameof(SpecificUserId)},{nameof(HasContextualLegalEntityAssociation)},{nameof(OperationId)}";
 
@@ -16,7 +20,7 @@ namespace Rights.Infra.Models
         public int OperationId { get; set; }
     }
 
-    internal class ApiKeyPermission : IApiKeyPermission
+    public class ApiKeyPermission : IApiKeyPermission
     {
         public static readonly string ApiFields = $"{nameof(LegalEntityId)},{nameof(Scope)},{nameof(ExternalEntityId)},{nameof(OperationId)}";
 
@@ -24,5 +28,102 @@ namespace Rights.Infra.Models
         public Scope Scope { get; set; }
         public int ExternalEntityId { get; set; }
         public int OperationId { get; set; }
+    }
+
+    public static class PermissionsExtensions
+    {
+        public static byte[] ToBytes(this Permission permission)
+        {
+            return ToByteArrays(permission).SelectMany(b => b).ToArray();
+        }
+        public static byte[] ToBytes(this List<Permission> permissions)
+        {
+            return permissions.SelectMany(ToByteArrays).SelectMany(b => b).ToArray();
+        }
+
+        private static IEnumerable<byte[]> ToByteArrays(IUserPermission permission)
+        {
+            yield return BitConverter.GetBytes((ushort)permission.OperationId);
+            yield return BitConverter.GetBytes((ushort)permission.Scope);
+            yield return BitConverter.GetBytes((ushort)permission.ExternalEntityId);
+            yield return BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? default));
+            yield return BitConverter.GetBytes((ushort)(permission.SpecificUserId ?? default));
+            yield return BitConverter.GetBytes((ushort)(permission.SpecificDepartmentId ?? default));
+            yield return BitConverter.GetBytes(permission.HasContextualLegalEntityAssociation);
+        }
+
+        public static Permission ToPermission(this byte[] bytes)
+        {
+            return new Permission
+            {
+                OperationId = BitConverter.ToUInt16(bytes, 0),
+                Scope = (Scope)BitConverter.ToUInt16(bytes, 2),
+                ExternalEntityId = BitConverter.ToUInt16(bytes, 4),
+                LegalEntityId = ParseNonDefault(bytes, 6),
+                SpecificUserId = ParseNonDefault(bytes, 8),
+                SpecificDepartmentId = ParseNonDefault(bytes, 10),
+                HasContextualLegalEntityAssociation = BitConverter.ToBoolean(bytes, 12)
+            };
+        }
+
+        public static List<Permission> ToPermissions(this byte[] bytes)
+        {
+            return bytes.ToBatches(13).Select(b => b.ToPermission()).ToList();
+        }
+
+        public static byte[] ToBytes(this ApiKeyPermission permission)
+        {
+            return ToByteArrays(permission).SelectMany(b => b).ToArray();
+        }
+
+        public static byte[] ToBytes(this List<ApiKeyPermission> permissions)
+        {
+            return permissions.SelectMany(ToByteArrays).SelectMany(b => b).ToArray();
+        }
+
+        private static IEnumerable<byte[]> ToByteArrays(ApiKeyPermission permission)
+        {
+            yield return BitConverter.GetBytes((ushort)permission.OperationId);
+            yield return BitConverter.GetBytes((ushort)permission.Scope);
+            yield return BitConverter.GetBytes((ushort)permission.ExternalEntityId);
+            yield return BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? 0));
+        }
+
+        public static ApiKeyPermission ToApiKeyPermission(this byte[] bytes)
+        {
+            return new ApiKeyPermission
+            {
+                OperationId = BitConverter.ToUInt16(bytes, 0),
+                Scope = (Scope)BitConverter.ToUInt16(bytes, 2),
+                ExternalEntityId = BitConverter.ToUInt16(bytes, 4),
+                LegalEntityId = ParseNonDefault(bytes, 6),
+            };
+        }
+
+        public static List<ApiKeyPermission> ToApiKeyPermissions(this byte[] bytes)
+        {
+            return bytes.ToBatches(8).Select(b => b.ToApiKeyPermission()).ToList();
+        }
+
+        private static ushort? ParseNonDefault(byte[] bytes, int index)
+        {
+            var parsedValue = BitConverter.ToUInt16(bytes, index);
+            return parsedValue == default
+                ? (ushort?)null
+                : parsedValue;
+        }
+
+        private static List<byte[]> ToBatches(this byte[] bytes, int length)
+        {
+
+            var batches = bytes.Batch(length).Select(b =>b.ToArray()).ToList();
+
+            if (batches.Last().Length != length)
+            {
+                throw new ApplicationException($"Byte array size is not a multiple of {length}");
+            }
+
+            return batches;
+        }
     }
 }
