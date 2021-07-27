aeb647a71ad43861ddd50e3fdb09b4e33b6cfe4c
Angelin Versini <aversini@lucca.fr>
2021-07-27 17:09:20 +0200

    MOD simplify UserRightsFilter

--- back/Core/Users/Users.Application/UserRightsFilter.cs
+++ back/Core/Users/Users.Application/UserRightsFilter.cs
@@ -1,5 +1,4 @@
 ﻿using Authentication.Domain;
-using Distributors.Domain;
 using Distributors.Domain.Models;
 using Rights.Domain.Filtering;
 using System;
@@ -13,27 +12,19 @@ namespace Users.Application
     // but it would result in a dependency loop between Authentication.Domain and Users.Domain
     public class UserRightsFilter
     {
-        private readonly IDistributorsStore _distributorsStore;
-
-        public UserRightsFilter(IDistributorsStore distributorsStore)
-        {
-            _distributorsStore = distributorsStore;
-        }
-
         public async Task<AccessRight> GetAccessAsync(ClaimsPrincipal principal)
         {
             return principal switch
             {
                 CloudControlApiKeyClaimsPrincipal _ => AccessRight.None,
-                CloudControlUserClaimsPrincipal user => await GetAccessAsync(user.User),
+                CloudControlUserClaimsPrincipal user => GetAccessRight(user.User),
                 _ => throw new ApplicationException("Unhandled ClaimsPrincipal type")
             };
         }
 
-        private async Task<AccessRight> GetAccessAsync(User user)
+        private AccessRight GetAccessRight(User user)
         {
-            var distributor = await _distributorsStore.GetByIdAsync(user.DistributorId);
-            return distributor.Id == DistributorIds.Lucca
+            return user.DistributorId == DistributorIds.Lucca
                 ? AccessRight.All
                 : AccessRight.ForDistributor(user.DistributorId);
         }
