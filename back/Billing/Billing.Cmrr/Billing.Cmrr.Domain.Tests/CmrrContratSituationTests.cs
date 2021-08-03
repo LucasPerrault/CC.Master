c48c6f2ce6b16bed6e50ee33833d8617fbdbd597
pierre bochu <40466697+boboch86@users.noreply.github.com>
2021-08-03 14:13:00 +0200

    Adapt tests for lifecycle according to specification in confluence

--- back/Billing/Billing.Cmrr/Billing.Cmrr.Domain.Tests/CmrrContratSituationTests.cs
+++ back/Billing/Billing.Cmrr/Billing.Cmrr.Domain.Tests/CmrrContratSituationTests.cs
@@ -1,4 +1,3 @@
-using Billing.Cmrr.Domain.Situation;
 using FluentAssertions;
 using System;
 using Xunit;
@@ -7,24 +6,6 @@ namespace Billing.Cmrr.Domain.Tests
 {
     public class CmrrContratSituationTests
     {
-        [Fact]
-        public void ShouldReturnContractSituationHasUpsell()
-        {
-            var contractSituation = new CmrrContractSituation(
-
-                new CmrrContract
-                {
-                    CreationCause = ContractCreationCause.NewBooking,
-                    EnvironmentCreatedAt = new DateTime(2021, 01, 01),
-                    StartDate = new DateTime(2022, 01, 01)
-                },
-                null,
-                new CmrrCount()
-            );
-
-            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Upsell);
-        }
-
         [Fact]
         public void ShouldReturnContractSituationHasCreation()
         {
@@ -44,7 +25,58 @@ namespace Billing.Cmrr.Domain.Tests
         }
 
         [Fact]
-        public void ShouldReturnContractSituationHasCreationWithContractModification()
+        public void ShouldReturnContractSituationHasUpsell()
+        {
+            var contractSituation = new CmrrContractSituation(
+
+                new CmrrContract
+                {
+                    CreationCause = ContractCreationCause.NewBooking,
+                    EnvironmentCreatedAt = new DateTime(2021, 01, 01),
+                    StartDate = new DateTime(2022, 01, 01)
+                },
+                null,
+                new CmrrCount()
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Upsell);
+        }
+
+        [Fact]
+        public void ShouldReturnContractSituationHasExpansion()
+        {
+            var contractSituation = new CmrrContractSituation(
+                new CmrrContract
+                {
+                    CreationCause = ContractCreationCause.NewBooking,
+                    StartDate = new DateTime(2022, 01, 01)
+                },
+                new CmrrCount { EuroTotal = 1000 },
+                new CmrrCount { EuroTotal = 1500 }
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Expansion);
+        }
+
+
+        [Fact]
+        public void ShouldReturnContractSituationHasExpansion2()
+        {
+            var contractSituation = new CmrrContractSituation(
+                new CmrrContract
+                {
+                    CreationCause = ContractCreationCause.Modification,
+                    StartDate = new DateTime(2022, 01, 01)
+                },
+                new CmrrCount { EuroTotal = 1000 },
+                new CmrrCount { EuroTotal = 1500 }
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Expansion);
+        }
+
+        [Fact]
+        public void ShouldReturnContractSituationHasExpansionWithContractModification()
         {
             var contractSitation = new CmrrContractSituation(
 
@@ -58,8 +90,51 @@ namespace Billing.Cmrr.Domain.Tests
                 new CmrrCount()
             );
 
-            contractSitation.LifeCycle.Should().Be(CmrrLifeCycle.Creation);
+            contractSitation.LifeCycle.Should().Be(CmrrLifeCycle.Expansion);
         }
+
+        [Fact]
+        public void ShouldReturnContractSituationHasContractionWithAmount()
+        {
+            var contractSituation = new CmrrContractSituation(
+                new CmrrContract
+                {
+                    EndReason = ContractEndReason.Resiliation,
+                },
+                new CmrrCount { EuroTotal = 1500 },
+                new CmrrCount { EuroTotal = 1000 }
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Contraction);
+        }
+
+        [Fact]
+        public void ShouldReturnContractSituationHasContractionWithEndPeriodCount()
+        {
+            var contractSituation = new CmrrContractSituation(
+                new CmrrContract
+                {
+                    EndReason = ContractEndReason.Modification,
+                },
+                new CmrrCount(),
+                new CmrrCount()
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Contraction);
+        }
+
+        [Fact]
+        public void ShouldReturnContractSituationHasContractionWithNoEndPeriodCount()
+        {
+            var contractSituation = new CmrrContractSituation(
+                new CmrrContract(),
+                new CmrrCount(),
+                null
+            );
+
+            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Contraction);
+        }
+
 
         [Fact]
         public void ShouldReturnContractSituationHasTermination()
@@ -76,49 +151,5 @@ namespace Billing.Cmrr.Domain.Tests
             contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Termination);
         }
 
-        [Fact]
-        public void ShouldReturnContractSituationHasContractionWithNoEndPeriodCount()
-        {
-            var contractSituation = new CmrrContractSituation(
-                new CmrrContract
-                {
-                    EndReason = ContractEndReason.Modification,
-                },
-                new CmrrCount(),
-                null
-            );
-
-            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Contraction);
-        }
-
-        [Fact]
-        public void ShouldReturnContractSituationHasContractionWithEndPeriodCount()
-        {
-            var contractSituation = new CmrrContractSituation(
-                new CmrrContract
-                {
-                    EndReason = ContractEndReason.Modification,
-                },
-                new CmrrCount { EuroTotal = 1500 },
-                new CmrrCount { EuroTotal = 1000 }
-            );
-
-            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Contraction);
-        }
-
-        [Fact]
-        public void ShouldReturnContractSituationHasExpansion()
-        {
-            var contractSituation = new CmrrContractSituation(
-                new CmrrContract
-                {
-                    EndReason = ContractEndReason.Modification,
-                },
-                new CmrrCount { EuroTotal = 1000 },
-                new CmrrCount { EuroTotal = 1500 }
-            );
-
-            contractSituation.LifeCycle.Should().Be(CmrrLifeCycle.Expansion);
-        }
     }
 }
