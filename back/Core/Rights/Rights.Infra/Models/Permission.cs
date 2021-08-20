8349f2cb2e192e3491d5f43f5653bd393e7807eb
Angelin Versini <aversini@lucca.fr>
2021-08-20 12:00:08 +0200

    MOD improve byte array copy performances

--- back/Core/Rights/Rights.Infra/Models/Permission.cs
+++ back/Core/Rights/Rights.Infra/Models/Permission.cs
@@ -31,75 +31,113 @@ namespace Rights.Infra.Models
 
     public static class PermissionsExtensions
     {
-        public static byte[] ToBytes(this Permission permission)
+        private static class UserPermissionBytes
         {
-            return ToByteArrays(permission).SelectMany(b => b).ToArray();
-        }
-        public static byte[] ToBytes(this List<IUserPermission> permissions)
-        {
-            return permissions.SelectMany(ToByteArrays).SelectMany(b => b).ToArray();
+            public const int TotalLength = 13;
+
+            public static class Positions
+            {
+                public const int OperationId = 0;
+                public const int Scope = 2;
+                public const int ExternalEntityId = 4;
+                public const int LegalEntityId = 6;
+                public const int SpecificUserId = 8;
+                public const int SpecificDepartmentId = 10;
+                public const int HasContextualLegalEntityAssociation = 12;
+            }
         }
 
-        private static IEnumerable<byte[]> ToByteArrays(IUserPermission permission)
+        public static byte[] ToBytes(this List<IUserPermission> permissions)
         {
-            yield return BitConverter.GetBytes((ushort)permission.OperationId);
-            yield return BitConverter.GetBytes((ushort)permission.Scope);
-            yield return BitConverter.GetBytes((ushort)permission.ExternalEntityId);
-            yield return BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? default));
-            yield return BitConverter.GetBytes((ushort)(permission.SpecificUserId ?? default));
-            yield return BitConverter.GetBytes((ushort)(permission.SpecificDepartmentId ?? default));
-            yield return BitConverter.GetBytes(permission.HasContextualLegalEntityAssociation);
+            var bytes = new byte[UserPermissionBytes.TotalLength * permissions.Count];
+            var offset = 0;
+            foreach (var permissionBytes in permissions.Select(p => p.ToBytes()))
+            {
+                Buffer.BlockCopy(permissionBytes, 0, bytes, offset, permissionBytes.Length);
+                offset += permissionBytes.Length;
+            }
+            return permissions.SelectMany(p => p.ToBytes()).ToArray();
+        }
+
+        public static byte[] ToBytes(this IUserPermission permission)
+        {
+            var bytes = new byte[UserPermissionBytes.TotalLength];
+            BitConverter.GetBytes((ushort)permission.OperationId).CopyTo(bytes, UserPermissionBytes.Positions.OperationId);
+            BitConverter.GetBytes((ushort)permission.Scope).CopyTo(bytes, UserPermissionBytes.Positions.Scope);
+            BitConverter.GetBytes((ushort)permission.ExternalEntityId).CopyTo(bytes, UserPermissionBytes.Positions.ExternalEntityId);
+            BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? default)).CopyTo(bytes, UserPermissionBytes.Positions.LegalEntityId);
+            BitConverter.GetBytes((ushort)(permission.SpecificUserId ?? default)).CopyTo(bytes, UserPermissionBytes.Positions.SpecificUserId);
+            BitConverter.GetBytes((ushort)(permission.SpecificDepartmentId ?? default)).CopyTo(bytes, UserPermissionBytes.Positions.SpecificDepartmentId);
+            BitConverter.GetBytes(permission.HasContextualLegalEntityAssociation).CopyTo(bytes, UserPermissionBytes.Positions.HasContextualLegalEntityAssociation);
+            return bytes;
         }
 
         public static Permission ToPermission(this byte[] bytes)
         {
             return new Permission
             {
-                OperationId = BitConverter.ToUInt16(bytes, 0),
-                Scope = (Scope)BitConverter.ToUInt16(bytes, 2),
-                ExternalEntityId = BitConverter.ToUInt16(bytes, 4),
-                LegalEntityId = ParseNonDefault(bytes, 6),
-                SpecificUserId = ParseNonDefault(bytes, 8),
-                SpecificDepartmentId = ParseNonDefault(bytes, 10),
-                HasContextualLegalEntityAssociation = BitConverter.ToBoolean(bytes, 12)
+                OperationId = BitConverter.ToUInt16(bytes, UserPermissionBytes.Positions.OperationId),
+                Scope = (Scope)BitConverter.ToUInt16(bytes, UserPermissionBytes.Positions.Scope),
+                ExternalEntityId = BitConverter.ToUInt16(bytes, UserPermissionBytes.Positions.ExternalEntityId),
+                LegalEntityId = ParseNonDefault(bytes, UserPermissionBytes.Positions.LegalEntityId),
+                SpecificUserId = ParseNonDefault(bytes, UserPermissionBytes.Positions.SpecificUserId),
+                SpecificDepartmentId = ParseNonDefault(bytes, UserPermissionBytes.Positions.SpecificDepartmentId),
+                HasContextualLegalEntityAssociation = BitConverter.ToBoolean(bytes, UserPermissionBytes.Positions.HasContextualLegalEntityAssociation)
             };
         }
 
         public static List<Permission> ToPermissions(this byte[] bytes)
         {
-            return bytes.ToBatches(13).Select(b => b.ToPermission()).ToList();
+            return bytes.ToBatches(UserPermissionBytes.TotalLength).Select(b => b.ToPermission()).ToList();
         }
 
-        public static byte[] ToBytes(this IApiKeyPermission permission)
+
+        private static class ApiKeyPermissionBytes
         {
-            return ToByteArrays(permission).SelectMany(b => b).ToArray();
+            public const int TotalLength = 6;
+
+            public static class Positions
+            {
+                public const int OperationId = 0;
+                public const int ExternalEntityId = 2;
+                public const int LegalEntityId = 4;
+            }
         }
 
         public static byte[] ToBytes(this List<IApiKeyPermission> permissions)
         {
-            return permissions.SelectMany(ToByteArrays).SelectMany(b => b).ToArray();
+            var bytes = new byte[ApiKeyPermissionBytes.TotalLength * permissions.Count];
+            var offset = 0;
+            foreach (var permissionBytes in permissions.Select(p => p.ToBytes()))
+            {
+                Buffer.BlockCopy(permissionBytes, 0, bytes, offset, permissionBytes.Length);
+                offset += permissionBytes.Length;
+            }
+            return permissions.SelectMany(p => p.ToBytes()).ToArray();
         }
 
-        private static IEnumerable<byte[]> ToByteArrays(IApiKeyPermission permission)
+        public static byte[] ToBytes(this IApiKeyPermission permission)
         {
-            yield return BitConverter.GetBytes((ushort)permission.OperationId);
-            yield return BitConverter.GetBytes((ushort)permission.ExternalEntityId);
-            yield return BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? 0));
+            var bytes = new byte[ApiKeyPermissionBytes.TotalLength];
+            BitConverter.GetBytes((ushort)permission.OperationId).CopyTo(bytes, ApiKeyPermissionBytes.Positions.OperationId);
+            BitConverter.GetBytes((ushort)permission.ExternalEntityId).CopyTo(bytes, ApiKeyPermissionBytes.Positions.ExternalEntityId);
+            BitConverter.GetBytes((ushort)(permission.LegalEntityId ?? 0)).CopyTo(bytes, ApiKeyPermissionBytes.Positions.LegalEntityId);
+            return bytes;
         }
 
         public static IApiKeyPermission ToApiKeyPermission(this byte[] bytes)
         {
             return new ApiKeyPermission
             {
-                OperationId = BitConverter.ToUInt16(bytes, 0),
-                ExternalEntityId = BitConverter.ToUInt16(bytes, 2),
-                LegalEntityId = ParseNonDefault(bytes, 4),
+                OperationId = BitConverter.ToUInt16(bytes, ApiKeyPermissionBytes.Positions.OperationId),
+                ExternalEntityId = BitConverter.ToUInt16(bytes, ApiKeyPermissionBytes.Positions.ExternalEntityId),
+                LegalEntityId = ParseNonDefault(bytes, ApiKeyPermissionBytes.Positions.LegalEntityId),
             };
         }
 
         public static List<IApiKeyPermission> ToApiKeyPermissions(this byte[] bytes)
         {
-            return bytes.ToBatches(6).Select(b => b.ToApiKeyPermission()).ToList();
+            return bytes.ToBatches(ApiKeyPermissionBytes.TotalLength).Select(b => b.ToApiKeyPermission()).ToList();
         }
 
         private static ushort? ParseNonDefault(byte[] bytes, int index)
