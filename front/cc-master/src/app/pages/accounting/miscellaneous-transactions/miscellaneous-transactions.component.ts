75dcac4a17efb1d7a127f95b933d3bcbed1a5e98
Perrault <lucas.perrault@hotmail.com>
2021-08-25 18:05:16 +0200

    init misc comp.

---
+++ front/cc-master/src/app/pages/accounting/miscellaneous-transactions/miscellaneous-transactions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cc-miscellaneous-transactions',
+  templateUrl: './miscellaneous-transactions.component.html',
+})
+export class MiscellaneousTransactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front/cc-master/src/app/pages/accounting/miscellaneous-transactions/miscellaneous-transactions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MiscellaneousTransactionsComponent } from './miscellaneous-transactions.component';
+
+
+
+@NgModule({
+  declarations: [
+    MiscellaneousTransactionsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MiscellaneousTransactionsModule { }
