348db5bfae3b6a7cd5bfce14a1c9df87166e032b
Perrault <lucas.perrault@hotmail.com>
2021-09-03 15:16:59 +0200

    front - add navigation tab

--- front/cc-master/src/app/common/navigation/constants/navigation-tabs.const.ts
+++ front/cc-master/src/app/common/navigation/constants/navigation-tabs.const.ts
@@ -126,30 +126,24 @@ export const navigationTabs: INavigationTab[] = [
       ],
       mode: OperationRestrictionMode.Some,
     },
-    children: [
-      {
-        url: NavigationPath.DistributorsManage,
-        name: 'front_navigation_distributorsManageTab',
-        isLegacy: true,
-      },
-      {
-        url: NavigationPath.DistributorsEnvironmentAccesses,
-        name: 'front_navigation_distributorsEnvironmentAccessesTab',
-        isLegacy: true,
-      },
-    ],
-  },
-  {
-    name: 'front_navigation_offersTab',
-    url: NavigationPath.Offers,
-    icon: 'pricetag',
-    restriction: { operations: [Operation.ReadCommercialOffers] },
-    isLegacy: true,
-  },
-  {
-    name: 'front_navigation_logsTab',
-    url: NavigationPath.Logs,
-    icon: 'list',
-    restriction: { operations: [Operation.EnvironmentLogTab] },
-  },
-];
+    {
+      name: 'front_navigation_offersTab',
+      url: NavigationPath.Offers,
+      icon: 'pricetag',
+      restriction: { operations: [Operation.ReadCommercialOffers] },
+      isLegacy: true,
+    },
+    {
+      name: 'front_navigation_logsTab',
+      url: NavigationPath.Logs,
+      icon: 'list',
+      restriction: { operations: [Operation.EnvironmentLogTab] },
+    },
+    {
+      name: 'front_navigation_cafeTab',
+      url: NavigationPath.Cafe,
+      icon: 'table',
+      // TODO : Add a restriction tab
+      restriction: { operations: [] },
+    },
+  ];
