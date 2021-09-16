4709c70dc0cfd9df7f5682860134aae2eda7732f
Angelin Versini <aversini@lucca.fr>
2021-09-16 18:21:06 +0200

    fix lint

--- front/cc-master/src/app/aspects/principal/principal.initializer.ts
+++ front/cc-master/src/app/aspects/principal/principal.initializer.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 import { IPrincipal } from './principal.interface';
 
