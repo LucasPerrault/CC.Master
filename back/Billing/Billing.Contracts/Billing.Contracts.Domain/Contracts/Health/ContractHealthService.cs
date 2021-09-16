32827b13d6399636acea73a18c3c747c6b62fc13
Perrault <lucas.perrault@hotmail.com>
2021-09-16 16:42:29 +0200

    ADD domain logic in contract health service

--- back/Billing/Billing.Contracts/Billing.Contracts.Domain/Contracts/Health/ContractHealthService.cs
+++ back/Billing/Billing.Contracts/Billing.Contracts.Domain/Contracts/Health/ContractHealthService.cs
@@ -1,5 +1,7 @@
-﻿using Billing.Contracts.Domain.Environments;
+﻿using System;
+using Billing.Contracts.Domain.Environments;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Billing.Contracts.Domain.Contracts.Health
 {
@@ -7,26 +9,70 @@ namespace Billing.Contracts.Domain.Contracts.Health
     {
         public List<ContractHealth> GetHealth(ContractEnvironment environment, List<Contract> contracts)
         {
-            //TODO implement environment health rules
-            return new List<ContractHealth>
+            return contracts
+                .Select(c => GetContractHealth(c, environment))
+                .ToList();
+        }
+
+        private ContractHealth GetContractHealth(Contract contract, ContractEnvironment environment)
+        {
+            return new ContractHealth
             {
-                new ContractHealth
-                {
-                    EnvironmentId = environment.Id,
-                    EstablishmentHealths = new List<EstablishmentHealth>
-                    {
-                        new EstablishmentHealth
-                        {
-                            EstablishmentId = 0,
-                            AttachmentErrors = new List<ContractAttachmentError>
-                            {
-                                ContractAttachmentError.MissingAttachment,
-                                ContractAttachmentError.InactiveAttachment
-                            }
-                        }
-                    }
-                }
+                ContractId = contract.Id,
+                EnvironmentId = environment.Id,
+                EstablishmentHealths = environment.Establishments
+                    .Select(e => GetEstablishmentHealth(e, contract.CommercialOffer.ProductId))
+                    .ToList()
             };
+        }
+
+        private EstablishmentHealth GetEstablishmentHealth(Establishment establishment, int productId)
+        {
+            return new EstablishmentHealth
+            {
+                EstablishmentId = establishment.Id,
+                AttachmentErrors = GetAttachmentErrors(establishment, productId),
+            };
+        }
+
+        private List<ContractAttachmentError> GetAttachmentErrors(Establishment establishment, int productId)
+        {
+            if (IsExcluded(establishment, productId))
+            {
+                return new List<ContractAttachmentError>();
+            }
+
+            if (!HasActiveAttachments(establishment))
+            {
+                return new List<ContractAttachmentError>() { ContractAttachmentError.MissingAttachment };
+            }
+
+            if (!establishment.IsActive)
+            {
+                return new List<ContractAttachmentError>() { ContractAttachmentError.InactiveAttachment };
+            }
+
+            return new List<ContractAttachmentError>();
+        }
+
+        private bool IsExcluded(Establishment establishment, int productId)
+        {
+            return establishment.Exclusions.Any(e => e.ProductId == productId);
+        }
+
+        private bool HasActiveAttachments(Establishment establishment)
+        {
+            return establishment.Attachments.Any(a => !IsFinished(a));
+        }
+
+        private bool IsFinished(EstablishmentAttachment attachment)
+        {
+            if (!attachment.EndsOn.HasValue)
+            {
+                return false;
+            }
+
+            return attachment.EndsOn > DateTime.Today;
         }
     }
 }
