3f64f506f2a901b8c512ee895128a9d90eaff520
Angelin Versini <aversini@lucca.fr>
2021-09-20 19:20:33 +0200

    ADD distributors api

---
+++ back/Shared/Distributors/Distributors.Application/DistributorsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Authentication.Domain;
+using Distributors.Domain;
+using Distributors.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Distributors.Application
+{
+    public class DistributorsRepository
+    {
+        private readonly IDistributorsStore _store;
+        private readonly ClaimsPrincipal _principal;
+
+        public DistributorsRepository(IDistributorsStore store, ClaimsPrincipal principal)
+        {
+            _store = store;
+            _principal = principal;
+        }
+
+        public async Task<List<Distributor>> GetAsync()
+        {
+            var distributors = await _store.GetAllAsync();
+            return _principal switch
+            {
+                CloudControlApiKeyClaimsPrincipal _ => distributors,
+                CloudControlUserClaimsPrincipal u when u.User.DistributorId == DistributorIds.Lucca => distributors,
+                CloudControlUserClaimsPrincipal u => distributors.Where(d => d.Id == u.User.DistributorId).ToList(),
+                _ => new List<Distributor>()
+            };
+        }
+    }
+}
