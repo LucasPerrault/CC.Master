22bb98847f15968a579cce5f7344ee81ba3ee3a4
Perrault <lucas.perrault@hotmail.com>
2021-09-23 15:49:33 +0200

    add offer api mapping service

---
+++ front/cc-master/src/app/pages/offers/services/offers-api-mapping.service.ts
@@ -0,0 +1,84 @@
+import { HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { apiV3SortToHttpParams, toApiV3SortParams } from '@cc/common/queries';
+import { ISortParams } from '@cc/common/sort';
+import { IProduct } from '@cc/domain/billing/offers';
+
+import { IBillingMode } from '../enums/billing-mode.enum';
+import { IOfferCurrency } from '../models/offer-currency.interface';
+import { IOfferFiltersForm } from '../models/offer-filters-form.interface';
+
+interface HttpParamsAttributes {
+  filters: IOfferFiltersForm;
+  sort: ISortParams;
+}
+
+enum OfferQueryParamKey {
+  Name = 'name',
+  Tag = 'tag',
+  ProductId = 'product.id',
+  Currency = 'currencyId',
+  BillingMode = 'billingMode',
+}
+
+@Injectable()
+export class OffersApiMappingService {
+  public toHttpParams(attributes: HttpParamsAttributes): HttpParams {
+    let params = new HttpParams();
+    params = this.setSortParams(params, attributes.sort);
+    return this.setOfferFilters(params, attributes.filters);
+  }
+
+  private setSortParams(params: HttpParams, sortParams: ISortParams): HttpParams {
+    if (!sortParams) {
+      return params;
+    }
+
+    const apiV3SortParams = toApiV3SortParams(sortParams);
+    return apiV3SortToHttpParams(params, apiV3SortParams);
+  }
+
+  private setOfferFilters(params: HttpParams, filters: IOfferFiltersForm): HttpParams {
+    if (!filters) {
+      return params;
+    }
+
+    params = this.getNameHttpParams(params, filters.search);
+    params = this.getTagHttpParams(params, filters.tag);
+    params = this.getProductHttpParams(params, filters.product);
+    params = this.getCurrencyHttpParams(params, filters.currencies);
+    params = this.getBillingModeHttpParams(params, filters.billingModes);
+    return params;
+  }
+
+  private getNameHttpParams(params: HttpParams, search: string): HttpParams {
+    return !!search
+      ? params.set(OfferQueryParamKey.Name, `like,${ encodeURIComponent(search) }`)
+      : params.delete(OfferQueryParamKey.Name);
+  }
+
+  private getTagHttpParams(params: HttpParams, tag: string): HttpParams {
+    return !!tag
+      ? params.set(OfferQueryParamKey.Tag, tag)
+      : params.delete(OfferQueryParamKey.Tag);
+  }
+
+  private getProductHttpParams(params: HttpParams, product: IProduct): HttpParams {
+    return !!product
+      ? params.set(OfferQueryParamKey.ProductId, product.id)
+      : params.delete(OfferQueryParamKey.ProductId);
+  }
+
+  private getCurrencyHttpParams(params: HttpParams, currencies: IOfferCurrency[]): HttpParams {
+    return !!currencies?.length
+      ? params.set(OfferQueryParamKey.Currency, currencies.map(c => c.code).join(','))
+      : params.delete(OfferQueryParamKey.Currency);
+  }
+
+  private getBillingModeHttpParams(params: HttpParams, billingModes: IBillingMode[]): HttpParams {
+    return !!billingModes?.length
+      ? params.set(OfferQueryParamKey.BillingMode, billingModes.map(b => b.id).join(','))
+      : params.delete(OfferQueryParamKey.BillingMode);
+  }
+
+}
