0339ef2052432f30b0a4d973872f5db90ff42625
Perrault <lucas.perrault@hotmail.com>
2021-09-28 09:52:49 +0200

    add offer billing unit select

--- front/cc-master/src/app/pages/offers/enums/billing-unit.enum.ts
+++ front/cc-master/src/app/pages/offers/enums/billing-unit.enum.ts
@@ -9,3 +9,49 @@ export enum BillingUnit {
   Licenses = 7,
   Servers = 8,
 }
+
+export interface IBillingUnit {
+  id: BillingUnit;
+  name: string;
+}
+
+export const billingUnits: IBillingUnit[] = [
+  {
+    id: BillingUnit.None,
+    name: 'offers_billingUnit_no',
+  },
+  {
+    id: BillingUnit.Users,
+    name: 'offers_billingUnit_users',
+  },
+  {
+    id: BillingUnit.ActiveUsers,
+    name: 'offers_billingUnit_activeUsers',
+  },
+  {
+    id: BillingUnit.Declarers,
+    name: 'offers_billingUnit_declarers',
+  },
+  {
+    id: BillingUnit.Cards,
+    name: 'offers_billingUnit_cards',
+  },
+  {
+    id: BillingUnit.DownloadedDocuments,
+    name: 'offers_billingUnit_downloadedDocuments',
+  },
+  {
+    id: BillingUnit.SynchronizedAccounts,
+    name: 'offers_billingUnit_synchronizedAccounts',
+  },
+  {
+    id: BillingUnit.Licenses,
+    name: 'offers_billingUnit_licenses',
+  },
+  {
+    id: BillingUnit.Servers,
+    name: 'offers_billingUnit_servers',
+  },
+];
+
+export const getBillingUnit = (id: BillingUnit): IBillingUnit => billingUnits.find(b => b.id === id);
