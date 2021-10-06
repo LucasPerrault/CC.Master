d1bc43155700c8cca0d6f345f882cc0968780297
Angelin Versini <aversini@lucca.fr>
2021-10-06 10:10:47 +0200

    MOD repare demos from the past

---
+++ back/Instances/Instances.Infra/Migrations/20211006075334_RepareDemos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Instances.Infra.Migrations
+{
+    public partial class RepareDemos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    update instances.Demos set cluster =
+                    (
+                        select top(1) targetCluster from instances.InstanceDuplications
+                            where progress = 3
+                            and sourceType = 3
+                            and startedAt > '2021-10-04'
+                            and targetSubdomain = instances.Demos.subdomain
+                            order by endedAt desc
+
+                    )
+                    where cluster is null
+");
+        }
+    }
+}
