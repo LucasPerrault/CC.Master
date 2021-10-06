169871fcd1e1d5427511fbc0887b3eeb8d5201f7
Angelin Versini <aversini@lucca.fr>
2021-10-06 17:40:20 +0200

    ADD foreign key

--- back/AdvancedFilters/AdvancedFilters.Infra/Storage/Configurations/LegalUnitsConfiguration.cs
+++ back/AdvancedFilters/AdvancedFilters.Infra/Storage/Configurations/LegalUnitsConfiguration.cs
@@ -27,7 +27,7 @@ namespace AdvancedFilters.Infra.Storage.Configurations
                 .IsRequired()
                 .OnDelete(DeleteBehavior.NoAction);
 
-            builder.Ignore(lu => lu.Country);
+            builder.HasOne(lu => lu.Country).WithMany().HasForeignKey(lu => lu.CountryId);
         }
     }
 }
