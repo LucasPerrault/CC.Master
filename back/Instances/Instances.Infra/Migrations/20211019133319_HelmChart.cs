d3da3f8f28edaf1afafbb6c0c3004d998c2dcc5a
Stéphane Seguin <sseguin@lucca.fr>
2021-10-19 17:32:33 +0200

    add migration script

---
+++ back/Instances/Instances.Infra/Migrations/20211019133319_HelmChart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Instances.Infra.Migrations
+{
+    public partial class HelmChart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "helmChart",
+                schema: "instances",
+                table: "GithubBranches",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "helmChart",
+                schema: "instances",
+                table: "GithubBranches");
+        }
+    }
+}
