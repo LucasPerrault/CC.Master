8424334161ebe64646764cf3bd43f73184089302
Angelin Versini <aversini@lucca.fr>
2021-10-23 01:02:29 +0200

    ADD generated sb migration

---
+++ back/Billing/Billing.Contracts/Billing.Contracts.Infra/Migrations/20211022225800_AlterAttachmentsAddEndReason.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Billing.Contracts.Infra.Migrations
+{
+    public partial class AlterAttachmentsAddEndReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+            (
+                $@"
+                ALTER VIEW [billing].[EstablishmentAttachments] WITH SCHEMABINDING AS
+                    SELECT
+                        ce.[id] Id,
+                        ce.[ContractId] ContractId,
+                        ce.[legalEntityId] EstablishmentId,
+                        le.[name] EstablishmentName,
+                        le.[localId] EstablishmentRemoteId,
+                        ce.[start] startsOn,
+                        ce.[end] endsOn,
+                        ce.[isActive] isActive,
+                        ce.[endReason] endReason
+                FROM [dbo].[ContractEntities] ce
+                    LEFT JOIN [dbo].[LegalEntities] le ON le.Id = ce.legalEntityID
+            "
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+            (
+                $@"
+                ALTER VIEW [billing].[EstablishmentAttachments] WITH SCHEMABINDING AS
+                    SELECT
+                        ce.[id] Id,
+                        ce.[ContractId] ContractId,
+                        ce.[legalEntityId] EstablishmentId,
+                        le.[name] EstablishmentName,
+                        le.[localId] EstablishmentRemoteId,
+                        ce.[start] startsOn,
+                        ce.[end] endsOn,
+                        ce.[isActive] isActive
+                FROM [dbo].[ContractEntities] ce
+                    LEFT JOIN [dbo].[LegalEntities] le ON le.Id = ce.legalEntityID
+            "
+            );
+        }
+    }
+}
