673e2536b792a21cc608f843f5c070de5918d42d
Perrault <lucas.perrault@hotmail.com>
2021-10-25 13:12:20 +0200

    fix lint

--- front/cc-master/src/app/common/forms/formly/formly.module.ts
+++ front/cc-master/src/app/common/forms/formly/formly.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { EnvironmentApiSelectModule, EnvironmentDomainSelectModule } from '@cc/common/forms';
-import { CountryApiSelectModule } from '@cc/common/forms/select/country-api-select/country-api-select.module';
+import {
+  CountryApiSelectModule,
+  EnvironmentApiSelectModule,
+  EnvironmentDomainSelectModule,
+  SpecializedContactRoleApiSelectModule,
+} from '@cc/common/forms';
 import { FormlyModule } from '@ngx-formly/core';
 
 import { FormlyFieldCountry } from './country/country.component';
@@ -10,7 +14,6 @@ import { FormlyFieldEnvironmentDomain } from './environment-domain/environment-d
 import { FormlyFieldEnvironmentSubdomain } from './environment-subdomain/environment-subdomain.component';
 import { luFormlyConfig } from './formly.config';
 import { FormlyFieldSpecializedContactRole } from './specialized-contact-role/specialized-contact-role.component';
-import { SpecializedContactRoleApiSelectModule } from '@cc/common/forms/select/specialized-contact-role-api-select/specialized-contact-role-api-select.module';
 
 @NgModule({
   declarations: [
--- front/cc-master/src/app/common/forms/index.ts
+++ front/cc-master/src/app/common/forms/index.ts
@@ -1,4 +1,5 @@
 export { ClientApiSelectModule } from './select/client-api-select/client-api-select.module';
+export { CountryApiSelectModule } from './select/country-api-select/country-api-select.module';
 export { DateRangeSelectModule } from './select/date-range-select';
 export { DistributorApiSelectModule } from './select/distributor-api-select/distributor-api-select.module';
 export { EnvironmentActionSelectModule } from './select/environment-action-select/environment-action-select.module';
@@ -8,6 +9,9 @@ export { EstablishmentApiSelectModule } from './select/establishment-api-select/
 export { OfferApiSelectModule } from './select/offer-api-select/offer-api-select.module';
 export { ProductApiSelectModule } from './select/product-api-select/product-api-select.module';
 export * from './select/select-display-mode.enum';
+export {
+  SpecializedContactRoleApiSelectModule,
+} from './select/specialized-contact-role-api-select/specialized-contact-role-api-select.module';
 export { UserApiSelectModule } from './select/user-api-select/user-api-select.module';
 export * from './state/button-state.const';
 export * from './state/submission-state.enum';
