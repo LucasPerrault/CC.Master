0a45508508610226327efb3ccb064e2e68e9d2a6
Alban Granger <agranger@lucca.fr>
2021-10-28 18:29:19 +0200

    MOD cluster+distribs+envAccess migrations

--- back/AdvancedFilters/AdvancedFilters.Infra/Storage/Configurations/EnvironmentAccessesConfiguration.cs
+++ back/AdvancedFilters/AdvancedFilters.Infra/Storage/Configurations/EnvironmentAccessesConfiguration.cs
@@ -8,15 +8,12 @@ namespace AdvancedFilters.Infra.Storage.Configurations
     {
         public void Configure(EntityTypeBuilder<EnvironmentAccess> builder)
         {
-            builder.ToTable("EnvironmentAccesss");
+            builder.ToTable("EnvironmentAccesses");
             builder.HasKey(c => c.Id);
             builder.Property(c => c.Id).ValueGeneratedNever();
 
             builder.Property(c => c.EnvironmentId).HasColumnName("EnvironmentId").IsRequired();
             builder.Property(c => c.DistributorId).HasColumnName("DistributorId").IsRequired();
-
-            builder.HasIndex(e => e.EnvironmentId);
-            builder.HasIndex(e => e.DistributorId);
 
             builder
                 .HasOne(a => a.Environment)
