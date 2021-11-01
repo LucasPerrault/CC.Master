f945f232117fca840248557be857d1f265ad5cd1
Guillaume Mouron <gmouron@lucca.fr>
2021-11-01 14:58:48 +0100

    [ADD] Méthode pour récupérer une instance d'un certain type via son environment id

--- back/Instances/Instances.Domain/Instances/IInstancesStore.cs
+++ back/Instances/Instances.Domain/Instances/IInstancesStore.cs
@@ -6,6 +6,7 @@ namespace Instances.Domain.Instances
 {
     public interface IInstancesStore
     {
+        Instance GetActiveInstanceFromEnvironmentId(int environmentId, InstanceType instanceType);
         Task<Instance> CreateForTrainingAsync(int environmentId, bool isAnonymized);
         Task<Instance> CreateForDemoAsync(string password);
         Task DeleteByIdAsync(int instanceId);
--- back/Instances/Instances.Infra/Storage/Stores/InstancesStore.cs
+++ back/Instances/Instances.Infra/Storage/Stores/InstancesStore.cs
@@ -23,6 +23,16 @@ namespace Instances.Infra.Storage.Stores
             _remoteStore = remoteStore;
         }
 
+        public Instance GetActiveInstanceFromEnvironmentId(int environmentId, InstanceType instanceType)
+        {
+            if (instanceType == InstanceType.Demo)
+            {
+                throw new BadRequestException($"You can't query an instance of type {instanceType} by environment id"); 
+            }
+
+            return Instances.Where(i => i.EnvironmentId == environmentId && i.IsActive && i.Type == instanceType).SingleOrDefault();
+        }
+
         public Task<Instance> CreateForTrainingAsync(int environmentId, bool isAnonymized)
         {
             return _remoteStore.CreateForTrainingAsync(environmentId, isAnonymized);
