0214c3e6794122344d1b11e751e72a212eaccc0b
Guillaume Mouron <gmouron@lucca.fr>
2021-11-05 14:55:59 +0100

    [REF] Renommage du projet Authentication.Tests en Authentication.Infra.Tests

--- back/Core/Authentication/Tests/Authentication.Infra.Tests/PrincipalStoreTests.cs
+++ back/Core/Authentication/Tests/Authentication.Infra.Tests/PrincipalStoreTests.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using Users.Domain;
 using Xunit;
 
-namespace Authentication.Tests
+namespace Authentication.Infra.Tests
 {
     public class PrincipalStoreTests
     {
