04f0b0ec5b1e326730e5aac41b6d0ea0fffa8fc2
Alban Granger <agranger@lucca.fr>
2021-11-05 18:10:17 +0100

    MOD lock sync/purge calls

---
+++ back/AdvancedFilters/AdvancedFilters.Web/Controllers/Locks/AdvancedFiltersSyncLock.cs
@@ -0,0 +1,8 @@
+namespace AdvancedFilters.Web.Controllers.Locks
+{
+    public static class AdvancedFiltersSyncLock
+    {
+        public const string Name = "cafe-sync";
+        public const int TimeoutInSeconds = 20;
+    }
+}
