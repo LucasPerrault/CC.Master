69f9e3bea74b9a6090b8fe2567f579e910eb6c03
Perrault <lperrault@lucca.fr>
2021-11-15 16:47:08 +0100

    add price lists tab comp.

---
+++ front/cc-master/src/app/pages/offers/components/offer-edition/offer-price-lists-tab/offer-price-lists-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cc-offer-price-lists-tab',
+  templateUrl: './offer-price-lists-tab.component.html',
+  styleUrls: ['./offer-price-lists-tab.component.scss']
+})
+export class OfferPriceListsTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
