464aa0e7c912f47f3ed170bcdcd383ea2be8ac38
Perrault <lperrault@lucca.fr>
2021-11-16 17:22:43 +0100

    front - mod import offer table to manage error

--- front/cc-master/src/app/pages/offers/components/offer-import/offer-import-table/offer-import-table.component.ts
+++ front/cc-master/src/app/pages/offers/components/offer-import/offer-import-table/offer-import-table.component.ts
@@ -1,33 +1,81 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 
-import { BillingMode, getBillingMode } from '../../../enums/billing-mode.enum';
-import { BillingUnit, getBillingUnit } from '../../../enums/billing-unit.enum';
-import { CurrencyCode, getCurrency } from '../../../models/offer-currency.interface';
-import { IImportedOffer } from '../imported-offer.interface';
+import { getBillingMode } from '../../../enums/billing-mode.enum';
+import { getBillingUnit } from '../../../enums/billing-unit.enum';
+import { getCurrency } from '../../../models/offer-currency.interface';
+import { IUploadedOffer } from '../uploaded-offer.interface';
+
+enum ImportedOfferFormKey {
+  Name = 'name',
+  Product ='product',
+  BillingUnit = 'billingUnit',
+  SageBusiness = 'sageBusiness',
+  Tag = 'tag',
+  BillingMode = 'billingMode',
+  Currency = 'currency',
+  ForecastMethod = 'forecastMethod',
+  PricingMethod = 'pricingMethod',
+  PriceLists = 'priceLists',
+}
 
 @Component({
   selector: 'cc-offer-import-table',
   templateUrl: './offer-import-table.component.html',
   styleUrls: ['./offer-import-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class OfferImportTableComponent {
-  @Input() public offers: IImportedOffer[];
+export class OfferImportTableComponent implements OnInit {
+  @Input() public set offers(importedOffers: IUploadedOffer[]) {
+    this.reset(importedOffers);
+  }
+
+  public formArrayKey = 'importedOffers';
+  public formArray: FormArray = new FormArray([]);
+  public formKey = ImportedOfferFormKey;
+  public formGroup: FormGroup = new FormGroup(
+    { [this.formArrayKey]: this.formArray },
+  );
 
   constructor() { }
 
-  public trackBy(index: number, offer: IImportedOffer): string {
-    return offer.name;
+  public ngOnInit(): void {
+
   }
 
-  public getBillingUnit(unit: BillingUnit): string {
-    return getBillingUnit(unit)?.name;
+  public trackBy(index: number, offer: AbstractControl): AbstractControl {
+    return offer;
   }
 
-  public getBillingMode(billingMode: BillingMode): string {
-    return getBillingMode(billingMode)?.name;
+  private reset(offers: IUploadedOffer[]): void {
+    this.formArray.clear();
+    this.addRange(offers);
   }
 
-  public getCurrencyName(code: CurrencyCode): string {
-    return getCurrency(code)?.name;
+  private addRange(offers: IUploadedOffer[]): void {
+    offers.forEach(offer => this.add(offer));
+  }
+
+  private add(offer: IUploadedOffer): void {
+    const formGroup = this.getFormGroup(offer);
+    this.formArray.push(formGroup);
+  }
+
+  private getFormGroup(offer: IUploadedOffer): FormGroup {
+    return new FormGroup(
+      {
+        [ImportedOfferFormKey.Name]: new FormControl(offer?.name),
+        [ImportedOfferFormKey.Product]: new FormControl(offer?.product),
+        [ImportedOfferFormKey.BillingUnit]: new FormControl(getBillingUnit(offer?.billingUnit)),
+        [ImportedOfferFormKey.SageBusiness]: new FormControl(offer?.sageBusiness),
+        [ImportedOfferFormKey.Tag]: new FormControl(offer?.category),
+        [ImportedOfferFormKey.BillingMode]: new FormControl(getBillingMode(offer?.billingMode)),
+        [ImportedOfferFormKey.PricingMethod]: new FormControl(offer?.pricingMethod),
+        [ImportedOfferFormKey.ForecastMethod]: new FormControl(offer?.forecastMethod),
+        [ImportedOfferFormKey.Currency]: new FormControl(getCurrency(offer?.currencyID)),
+        [ImportedOfferFormKey.PriceLists]: new FormControl(offer?.priceLists),
+      },
+    );
   }
 }
--- front/cc-master/src/app/pages/offers/components/offer-import/offer-import.module.ts
+++ front/cc-master/src/app/pages/offers/components/offer-import/offer-import.module.ts
@@ -1,11 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@cc/aspects/translate';
+import { ProductApiSelectModule } from '@cc/common/forms';
 
 import { OfferPageTemplateModule } from '../offer-page-template/offer-page-template.module';
+import {
+  OfferBillingModeSelectModule,
+  OfferBillingUnitSelectModule,
+  OfferCurrencySelectModule,
+  OfferForecastMethodApiSelectModule,
+  OfferPricingMethodApiSelectModule,
+  OfferTagApiSelectModule,
+} from '../offer-selects';
 import { OfferImportComponent } from './offer-import.component';
-import { OfferUploadComponent } from './offer-upload/offer-upload.component';
 import { OfferImportTableComponent } from './offer-import-table/offer-import-table.component';
+import { OfferUploadComponent } from './offer-upload/offer-upload.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +27,14 @@ import { OfferImportTableComponent } from './offer-import-table/offer-import-tab
     CommonModule,
     OfferPageTemplateModule,
     TranslateModule,
+    ProductApiSelectModule,
+    ReactiveFormsModule,
+    OfferBillingUnitSelectModule,
+    OfferTagApiSelectModule,
+    OfferBillingModeSelectModule,
+    OfferPricingMethodApiSelectModule,
+    OfferForecastMethodApiSelectModule,
+    OfferCurrencySelectModule,
   ],
 })
 export class OfferImportModule { }
