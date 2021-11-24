548c75773cb0afdf8c7ff00d76cc5ed0d0001dd4
Perrault <lperrault@lucca.fr>
2021-11-24 17:14:40 +0100

    add offer tab data service

---
+++ front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/offer-tab/services/offer-tab-data.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CountsService, ICount } from '@cc/domain/billing/counts';
+import { Observable } from 'rxjs';
+
+import { IOfferContract } from '../models/offer-contract.interface';
+
+@Injectable()
+export class OfferTabDataService {
+  constructor(private httpClient: HttpClient, private countsService: CountsService) {
+  }
+
+  public getRealCounts$(contractId: number): Observable<ICount[]> {
+    return this.countsService.getRealCounts$(contractId);
+  }
+
+  public getContract$(contractId: number): Observable<IOfferContract> {
+    const url = `/api/contracts/${ contractId }`;
+    return this.httpClient.get<IOfferContract>(url);
+  }
+}
