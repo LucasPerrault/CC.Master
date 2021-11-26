1f2300ceed116914664d404a5256020fd1827b06
Perrault <lperrault@lucca.fr>
2021-11-26 11:08:33 +0100

    add similar offer select in offer tab

--- front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/offer-tab/offer-tab.component.ts
+++ front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/offer-tab/offer-tab.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { TimelineCountsService } from '@cc/domain/billing/counts/timeline-counts-service';
+import { forkJoin, Observable, ReplaySubject } from 'rxjs';
+import { finalize, map, take } from 'rxjs/operators';
+
+import { IOfferContract } from './models/offer-contract.interface';
+import { ISimilarOfferContext } from './models/similar-offer-context.interface';
+import { OfferTabDataService } from './services/offer-tab-data.service';
 
 @Component({
   selector: 'cc-offer-tab',
   templateUrl: './offer-tab.component.html',
-  styleUrls: ['./offer-tab.component.scss']
+  styleUrls: ['./offer-tab.component.scss'],
 })
 export class OfferTabComponent implements OnInit {
+  public isLoading$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
-  constructor() { }
+  public formControl: FormControl = new FormControl();
+  public context$: ReplaySubject<ISimilarOfferContext> = new ReplaySubject<ISimilarOfferContext>(1);
 
-  ngOnInit(): void {
+  public get contractId(): number {
+    return parseInt(this.activatedRoute.parent.snapshot.paramMap.get('id'), 10);
   }
 
+  constructor(private activatedRoute: ActivatedRoute, private dataService: OfferTabDataService) { }
+
+  public ngOnInit(): void {
+    this.isLoading$.next(true);
+
+    const requests$ = [
+      this.dataService.getContract$(this.contractId),
+      this.getLastCountPeriod$(this.contractId),
+    ];
+
+    forkJoin(requests$)
+      .pipe(take(1), finalize(() => this.isLoading$.next(false)))
+      .subscribe(([contract, lastCountPeriod]: [IOfferContract, Date]) => this.setSimilarOfferContext(contract, lastCountPeriod));
+  }
+
+  private getLastCountPeriod$(contractId: number): Observable<Date> {
+    return this.dataService.getRealCounts$(contractId)
+      .pipe(map(counts => TimelineCountsService.getLastCountPeriod(counts)));
+  }
+
+  private setSimilarOfferContext(contract: IOfferContract, lastCountPeriod: Date): void {
+    const offerId = contract.commercialOfferId;
+    const maxPeriod = lastCountPeriod ?? new Date(contract.theoreticalStartOn);
+
+    this.context$.next({ offerId, maxPeriod });
+  }
 }
--- front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/offer-tab/offer-tab.module.ts
+++ front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/offer-tab/offer-tab.module.ts
@@ -2,6 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { OfferTabComponent } from './offer-tab.component';
+import { OfferTabDataService } from './services/offer-tab-data.service';
+import { LuApiSelectInputModule } from '@lucca-front/ng/api';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LuTooltipTriggerModule } from '@lucca-front/ng/tooltip';
+import { TranslateModule } from '@cc/aspects/translate';
+import { SimilarOfferApiSelectModule } from './components/similar-offer-api-select/similar-offer-api-select.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +15,12 @@ import { OfferTabComponent } from './offer-tab.component';
   ],
   imports: [
     CommonModule,
+    LuApiSelectInputModule,
+    ReactiveFormsModule,
+    LuTooltipTriggerModule,
+    TranslateModule,
+    SimilarOfferApiSelectModule,
   ],
+  providers: [OfferTabDataService],
 })
 export class OfferTabModule { }
