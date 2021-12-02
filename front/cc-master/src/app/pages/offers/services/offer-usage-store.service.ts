43fc57efd1aa181cfccdf82c25d1791ca43837dd
Perrault <lperrault@lucca.fr>
2021-12-02 14:22:19 +0100

    fix offers stored with usage

--- front/cc-master/src/app/pages/offers/services/offer-usage-store.service.ts
+++ front/cc-master/src/app/pages/offers/services/offer-usage-store.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
 import { IOfferUsage } from '../models/offer-usage.interface';
 import { OffersDataService } from './offers-data.service';
 
+interface IOfferStored {
+  offerId: number;
+  usage: IOfferUsage;
+}
+
 @Injectable()
 export class OfferUsageStoreService {
-  private usages$: BehaviorSubject<IOfferUsage[]> = new BehaviorSubject<IOfferUsage[]>([]);
+  private offersStored: BehaviorSubject<IOfferStored[]> = new BehaviorSubject<IOfferStored[]>([]);
 
   constructor(private dataService: OffersDataService) {
   }
@@ -17,20 +22,31 @@ export class OfferUsageStoreService {
   }
 
   public getUsages$(offerIds: number[]): Observable<IOfferUsage[]> {
-    const allOfferIdsWithUsage = this.usages$.value?.map(u => u.offerId);
+    const allOfferIdsWithUsage = this.offersStored.value?.map(store => store.offerId);
     const offerIdsWithoutUsage = offerIds.filter(offerId => !allOfferIdsWithUsage.includes(offerId));
 
+    if (!offerIdsWithoutUsage.length) {
+      return of(this.getUsages(offerIds));
+    }
+
     return this.dataService.getUsages$(offerIdsWithoutUsage).pipe(
-      tap(usages => this.add(usages)),
+      tap(usages => this.add(offerIds, usages)),
       map(() => this.getUsages(offerIds)),
     );
   }
 
   private getUsages(offerIds: number[]): IOfferUsage[] {
-    return this.usages$.value?.filter(u => offerIds.includes(u.offerId)) ?? [];
+    const offers = this.offersStored.value?.filter(s => offerIds.includes(s.offerId)) ?? [];
+    return offers.map(o => o.usage);
   }
 
-  private add(usages: IOfferUsage[]): void {
-    this.usages$.next([...this.usages$.value, ...usages]);
+  private add(offerIds: number[], usages: IOfferUsage[]): void {
+    const offersToStore = offerIds.map(offerId => this.toOfferStored(offerId, usages));
+    this.offersStored.next([...this.offersStored.value, ...offersToStore]);
+  }
+
+  private toOfferStored(offerId: number, usages: IOfferUsage[]): IOfferStored {
+    const usage = usages.find(u => u.offerId === offerId);
+    return { offerId, usage };
   }
 }
