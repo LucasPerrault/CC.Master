b44088acb782b05fb891cf2d7d572ddf1e1ad5ef
Angelin Versini <angelin.versini@gmail.com>
2021-12-08 11:45:47 +0100

    MOD sanitize price lists starts on

---
+++ back/Billing/Billing.Contracts/Billing.Contracts.Infra/Migrations/20211208103943_UpdatePriceListsSanitizeStartsOn.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Billing.Contracts.Infra.Migrations
+{
+    public partial class UpdatePriceListsSanitizeStartsOn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                update billing.PriceLists
+                set StartsOn = '2002-01-01'
+                where StartsOn < '2002-01-01'
+");
+        }
+    }
+}
