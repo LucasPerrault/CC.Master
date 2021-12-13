b655597e6f1bb6d13ae2b4cd89ac9113855b2151
Perrault <lperrault@lucca.fr>
2021-12-13 11:38:14 +0100

    mod jest config test for custom path

--- front/cc-master/jest.config.js
+++ front/cc-master/jest.config.js
@@ -14,6 +14,9 @@ module.exports = {
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
+  moduleNameMapper: {
+    "@cc/(.*)": "<rootDir>/src/app/$1"
+  },
   coverageDirectory: 'results',
   collectCoverageFrom: ['<rootDir>/src/app/**/*.ts', '!<rootDir>/src/app/**/*.module.ts', '!<rootDir>/src/app/**/index.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', 'translations.ts'],
