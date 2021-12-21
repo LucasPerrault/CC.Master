0b085d32cbea9510f0a8f25481afa754424007ea
Perrault <lperrault@lucca.fr>
2021-12-21 15:31:05 +0100

    [cafe] add billing entity on environment client in advanced filters

--- back/AdvancedFilters/AdvancedFilters.Domain/Billing/Models/Client.cs
+++ back/AdvancedFilters/AdvancedFilters.Domain/Billing/Models/Client.cs
@@ -5,11 +5,18 @@ using Tools;
 
 namespace AdvancedFilters.Domain.Billing.Models
 {
+    public enum BillingEntity
+    {
+        Unknown = 0,
+        France = 1,
+        Iberia = 2,
+    }
     public class Client : IDeepCopyable<Client>
     {
         public int Id { get; set; }
         public Guid ExternalId { get; set; }
         public string Name { get; set; }
+        public BillingEntity BillingEntity { get; set; }
 
         public IEnumerable<Contract> Contracts { get; set; }
 
---
+++ back/AdvancedFilters/AdvancedFilters.Infra/Migrations/20211221144634_UpdateClientsAddBillingEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedFilters.Infra.Migrations
+{
+    public partial class UpdateClientsAddBillingEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BillingEntity",
+                schema: "cafe",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingEntity",
+                schema: "cafe",
+                table: "Clients");
+        }
+    }
+}
