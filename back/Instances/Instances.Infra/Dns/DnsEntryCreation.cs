2873e50f1cd0d165bc599e0d207f11a5c309f6d6
Guillaume Mouron <gmouron@lucca.fr>
2022-01-04 15:15:08 +0100

    [REF] On améliore la structure des classes pour les entrées DNS

--- back/Instances/Instances.Infra/Dns/DnsEntryCreation.cs
+++ back/Instances/Instances.Infra/Dns/DnsEntryCreation.cs
@@ -4,21 +4,21 @@ namespace Instances.Infra.Dns
 {
     public interface IDnsEntry
     {
-        public string Subdomain { get; set; }
-        public string DnsZone { get; set; }
+        public string Subdomain { get; }
+        public string DnsZone { get; }
     }
 
     public class DnsEntryCreation : IDnsEntry
     {
-        public string Subdomain { get; set; }
-        public string DnsZone { get; set; }
-        public string Cluster { get; set; }
+        public string Subdomain { get; init; }
         public DnsEntryZone DnsEntryZone { get; init; }
+        public string DnsZone { get; init; }
+        public string Cluster { get; init; }
     }
 
     public class DnsEntryDeletion : IDnsEntry
     {
-        public string Subdomain { get; set; }
-        public string DnsZone { get; set; }
+        public string Subdomain { get; init; }
+        public string DnsZone { get; init; }
     }
 }
