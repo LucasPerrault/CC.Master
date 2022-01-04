7b51cf961acb971dcac22c7b036da60a2d63c2a7
Perrault <lperrault@lucca.fr>
2022-01-04 08:41:10 +0100

    fix set disabled$ by default

--- front/cc-master/src/app/pages/offers/components/editable-price-grid/editable-price-grid.component.ts
+++ front/cc-master/src/app/pages/offers/components/editable-price-grid/editable-price-grid.component.ts
@@ -10,7 +10,7 @@ import {
   ValidationErrors,
   Validator,
 } from '@angular/forms';
-import { BehaviorSubject, combineLatest, Observable, ReplaySubject, Subject } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
 import { filter, map, takeUntil } from 'rxjs/operators';
 
 import { IPriceRowForm } from '../../models/price-list-form.interface';
@@ -61,7 +61,7 @@ export class EditablePriceGridComponent implements OnInit, OnDestroy, ControlVal
   @ViewChild('tableElement') public tableElement: ElementRef<HTMLTableElement>;
 
   public readonly$: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  public disabled$: ReplaySubject<boolean> = new ReplaySubject(1);
+  public disabled$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   public readonlyFormControls: AbstractControl[] = [];
 
   public get showAddPriceRowButton(): boolean {
