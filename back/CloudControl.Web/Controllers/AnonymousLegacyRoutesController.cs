f908a84624142c67867c567cb0e1cd74b66b1ca1
versinia <angelin.versini@gmail.com>
2022-01-06 12:45:40 +0100

    ADD declare anonymous routes of legacy CC

---
+++ back/CloudControl.Web/Controllers/AnonymousLegacyRoutesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+
+namespace CloudControl.Web.Controllers;
+
+[AllowAnonymous]
+public class AnonymousLegacyRoutesController
+{
+    [HttpPost("api/v3/opportunitiesSync")]
+    public HttpResponseMessage AllowAnonymousCallToLegacy()
+        => throw new ApplicationException("Calls to this route should be intercepted by proxy");
+}
