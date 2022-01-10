1d14412e6309545faa4b402feeb5493ea994ee57
versinia <angelin.versini@gmail.com>
2022-01-10 12:27:48 +0100

    FIX api is not paginated anymore ; it cannot sort back-side

--- front/cc-master/src/app/pages/cafe/common/forms/select/distributor-api-select/distributor-api-select.service.ts
+++ front/cc-master/src/app/pages/cafe/common/forms/select/distributor-api-select/distributor-api-select.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { LuApiV4Service } from '@lucca-front/ng/api';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { IDistributor } from '../../../../environments/models/environment-access.interface';
+
+const sortDistributors = (distributors: IDistributor[]) => distributors
+    .sort((d1, d2) => d1.name.localeCompare(d2.name))
+    .sort(d => d.isLucca  ? -1 : 1);
 
 @Injectable()
 export class DistributorApiSelectService extends LuApiV4Service<IDistributor> {
 
   public getAll(filters?: string[]): Observable<IDistributor[]> {
-    return super.getAll([...filters, 'sort=-isLucca,name']);
+    return super.getAll(filters).pipe(map(sortDistributors));
   }
 
   searchAll(clue?: string, filters?: string[]): Observable<IDistributor[]> {
-    return super.searchAll(clue, [...filters, 'sort=-isLucca,name']);
+    return super.searchAll(clue, filters).pipe(map(sortDistributors));
   }
 }
