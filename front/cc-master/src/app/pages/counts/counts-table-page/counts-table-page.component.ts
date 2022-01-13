d941420a3d6bcd8a5518733ea33828c3c9105899
Perrault <lperrault@lucca.fr>
2022-01-13 14:44:45 +0100

    add list + filters on counts table page

--- front/cc-master/src/app/pages/counts/counts-table-page/counts-table-page.component.ts
+++ front/cc-master/src/app/pages/counts/counts-table-page/counts-table-page.component.ts
@@ -1,15 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IPaginatedResult, PaginatedList, PaginatedListState, PagingService } from '@cc/common/paging';
+import { ISortParams, SortOrder } from '@cc/common/sort';
+import { endOfMonth, startOfMonth, subMonths } from 'date-fns';
+import { BehaviorSubject, combineLatest, Observable, ReplaySubject, Subject } from 'rxjs';
+import { debounceTime, map, shareReplay, skip, startWith, takeUntil, tap } from 'rxjs/operators';
+
+import { CountAdditionalColumn, defaultColumnsDisplayed } from './components/count-additional-column-select/count-additional-column.enum';
+import { CountsSortParamKey } from './enums/count-sort-param-key.enum';
+import { CountsFilterFormKey } from './models/counts-filter-form.interface';
+import { IDetailedCount } from './models/detailed-count.interface';
+import { CountsApiMappingService } from './services/counts-api-mapping.service';
+import { CountsDataService } from './services/counts-data.service';
+import { IContractAdditionalColumn } from '../../contracts/contracts-manage/constants/contract-additional-column.enum';
 
 @Component({
   selector: 'cc-counts-table-page',
   templateUrl: './counts-table-page.component.html',
   styleUrls: ['./counts-table-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountsTablePageComponent implements OnInit {
+export class CountsTablePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public get counts$(): Observable<IDetailedCount[]> {
+    return this.paginatedCounts.items$;
+  }
 
-  ngOnInit(): void {
+  public get totalCount$(): Observable<number> {
+    return this.paginatedCounts.totalCount$;
+  }
+
+  public get state$(): Observable<PaginatedListState> {
+    return this.paginatedCounts.state$;
+  }
+
+  public isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  public filters: FormControl = new FormControl();
+  public sortParams$: BehaviorSubject<ISortParams> = new BehaviorSubject<ISortParams>(null);
+
+  public columnsSelectedFormControl: FormControl = new FormControl();
+  public columnsSelected$: ReplaySubject<CountAdditionalColumn[]> = new ReplaySubject(1);
+
+  private paginatedCounts: PaginatedList<IDetailedCount>;
+  private destroy$: Subject<void> = new Subject();
+
+  constructor(
+    private pagingService: PagingService,
+    private dataService: CountsDataService,
+    private apiMappingService: CountsApiMappingService,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.initDefaultFiltersAndSort();
+
+    combineLatest([this.filters.valueChanges, this.sortParams$])
+      .pipe(
+        takeUntil(this.destroy$),
+        debounceTime(300),
+        map(([filters, sort]) => ({ filters, sort })),
+        map(attributes => this.apiMappingService.toHttpParams(attributes)),
+      )
+      .subscribe(httpParams => this.paginatedCounts.updateHttpParams(httpParams));
+
+    this.columnsSelectedFormControl.valueChanges
+      .pipe(map(columns => !!columns ? columns.map(c => c.id) : []))
+      .subscribe(this.columnsSelected$);
+
+    this.columnsSelectedFormControl.setValue(defaultColumnsDisplayed);
+
+    this.paginatedCounts = this.pagingService.paginate<IDetailedCount>(
+      (httpParams) => this.getPaginatedCounts$(httpParams),
+    );
+
+    this.paginatedCounts.state$
+      .pipe(skip(1), map(state => state === PaginatedListState.Update))
+      .subscribe(this.isLoading$);
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public nextPage(): void {
+    this.paginatedCounts.nextPage();
+  }
+
+  public sort(sortParams: ISortParams) {
+    this.sortParams$.next(sortParams);
+  }
+
+  private getPaginatedCounts$(httpParams: HttpParams): Observable<IPaginatedResult<IDetailedCount>> {
+    return this.dataService.getDetailedCounts$(httpParams).pipe(
+      map(response => ({ items: response.items, totalCount: response.count })),
+    );
+  }
+
+  private initDefaultFiltersAndSort(): void {
+    this.sortParams$.next({ field: CountsSortParamKey.Client, order: SortOrder.Asc });
+    this.filters.setValue({
+      [CountsFilterFormKey.CountPeriod]: {
+        startDate: subMonths(startOfMonth(Date.now()), 1),
+        endDate: subMonths(endOfMonth(Date.now()), 1),
+      },
+    });
   }
 
 }
--- front/cc-master/src/app/pages/counts/counts-table-page/counts-table-page.module.ts
+++ front/cc-master/src/app/pages/counts/counts-table-page/counts-table-page.module.ts
@@ -1,20 +1,37 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { CountsTablePageComponent } from './counts-table-page.component';
-import { CountsPageTemplateModule } from '../common/counts-page-template/counts-page-template.module';
-import { CountsListComponent } from './components/counts-list/counts-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@cc/aspects/translate';
+import { PagingModule } from '@cc/common/paging';
+import { LuTooltipTriggerModule } from '@lucca-front/ng/tooltip';
+
+import { CountsPageTemplateModule } from '../common/counts-page-template/counts-page-template.module';
+import { CountsFilterModule } from './components/counts-filter/counts-filter.module';
+import { CountsListComponent } from './components/counts-list/counts-list.component';
+import { CountsTablePageComponent } from './counts-table-page.component';
+import { CountsApiMappingService } from './services/counts-api-mapping.service';
+import { CountsDataService } from './services/counts-data.service';
+import { CountAdditionalColumnSelectModule } from './components/count-additional-column-select/count-additional-column-select.module';
 
 @NgModule({
   declarations: [
     CountsTablePageComponent,
     CountsListComponent,
   ],
-  imports: [
-    CommonModule,
-    CountsPageTemplateModule,
-    TranslateModule,
+    imports: [
+        CommonModule,
+        CountsPageTemplateModule,
+        TranslateModule,
+        PagingModule,
+        LuTooltipTriggerModule,
+        CountsFilterModule,
+        ReactiveFormsModule,
+        CountAdditionalColumnSelectModule,
+    ],
+  providers: [
+    CountsDataService,
+    CountsApiMappingService,
+    CurrencyPipe,
   ],
 })
 export class CountsTablePageModule { }
