f28a235a43e4c3b3d033c27e40593688c51199e1
Perrault <lperrault@lucca.fr>
2022-01-18 18:49:20 +0100

    add counts dashboard card

---
+++ front/cc-master/src/app/pages/counts/counts-launcher/components/counts-dashboard-card-list/counts-dashboard-card/counts-dashboard-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'cc-counts-dashboard-card',
+  templateUrl: './counts-dashboard-card.component.html',
+  styleUrls: ['./counts-dashboard-card.component.scss'],
+})
+export class CountsDashboardCardComponent {
+  @Input() public mod: string;
+  @Input() public titleTranslationKey: number;
+  @Input() public totalCount: number;
+  @Input() public isLoading: boolean;
+  @Input() public isSelected: boolean;
+
+  @Output() public showMoreDetails: EventEmitter<void> = new EventEmitter();
+
+  constructor() { }
+
+}
