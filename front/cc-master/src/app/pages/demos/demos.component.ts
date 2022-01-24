046d9863f2ccf62f9ce2e98007759cd7c07d88fb
Perrault <lperrault@lucca.fr>
2022-01-24 11:10:05 +0100

    add demos card list and filters

--- front/cc-master/src/app/pages/demos/demos.component.ts
+++ front/cc-master/src/app/pages/demos/demos.component.ts
@@ -1,15 +1,119 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IPrincipal, PRINCIPAL } from '@cc/aspects/principal';
+import { defaultPagingParams, IPaginatedResult, PaginatedList, PaginatedListState, PagingService } from '@cc/common/paging';
+import { ApiStandard } from '@cc/common/queries';
+import { IUser } from '@cc/domain/users/v4';
+import { LuModal } from '@lucca-front/ng/modal';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { map, skip, take, takeUntil } from 'rxjs/operators';
+
+import { DemoCommentModalComponent } from './components/modals/demo-comment-modal/demo-comment-modal.component';
+import { DemoCommentModalMode } from './components/modals/demo-comment-modal/demo-comment-modal-data.interface';
+import { DemoDeletionModalComponent } from './components/modals/demo-deletion-modal/demo-deletion-modal.component';
+import { DemoPasswordEditionModalComponent } from './components/modals/demo-password-edition-modal/demo-password-edition-modal.component';
+import { IDemo, ITemplateDemo } from './models/demo.interface';
+import { DemoFilterFormKey } from './models/demo-filters.interface';
+import { DemosApiMappingService } from './services/demos-api-mapping.service';
+import { DemosDataService } from './services/demos-data.service';
+import { DemosListService } from './services/demos-list.service';
 
 @Component({
   selector: 'cc-demos',
   templateUrl: './demos.component.html',
   styleUrls: ['./demos.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DemosComponent implements OnInit {
+export class DemosComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public get demos$(): Observable<IDemo[]> { return this.paginatedDemos.items$; }
+  public isLoading$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
+  public isLoadingMore$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
-  ngOnInit(): void {
+  public templateDemos$ = new ReplaySubject<ITemplateDemo[]>(1);
+  public filters = new FormControl();
+
+  private paginatedDemos: PaginatedList<IDemo>;
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    @Inject(PRINCIPAL) private principal: IPrincipal,
+    private pagingService: PagingService,
+    private dataService: DemosDataService,
+    private apiMappingService: DemosApiMappingService,
+    private listService: DemosListService,
+    private luModal: LuModal,
+  ) { }
+
+  public ngOnInit(): void {
+    this.dataService.getTemplateDemos$()
+      .pipe(take(1))
+      .subscribe(this.templateDemos$);
+
+    this.filters.valueChanges
+      .pipe(takeUntil(this.destroy$), map(f => this.apiMappingService.toHttpParams(f)))
+      .subscribe(httpParams => this.paginatedDemos.updateHttpParams(httpParams));
+
+    this.listService.resetAll$
+      .pipe(takeUntil(this.destroy$), map(() => this.apiMappingService.toHttpParams(this.filters.value)))
+      .subscribe(httpParams => this.paginatedDemos.updateHttpParams(httpParams));
+
+    this.paginatedDemos = this.pagingService.paginate<IDemo>(
+      (httpParams) => this.getDemos$(httpParams),
+      { page: defaultPagingParams.page, limit: 20 },
+      ApiStandard.V4,
+    );
+
+    this.paginatedDemos.state$
+      .pipe(skip(1), map(state => state === PaginatedListState.Update))
+      .subscribe(this.isLoading$);
+
+    this.paginatedDemos.state$
+      .pipe(map(state => state === PaginatedListState.LoadMore))
+      .subscribe(this.isLoadingMore$);
+
+    this.initDefaultFilter();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public nextPage(): void {
+    this.paginatedDemos.nextPage();
+  }
+
+  public openDeletionModal(demo: IDemo): void {
+    this.luModal.open(DemoDeletionModalComponent, demo);
+  }
+
+  public openCommentModal(demo: IDemo): void {
+    const mode = DemoCommentModalMode.Readonly;
+    this.luModal.open(DemoCommentModalComponent, { demo, mode });
+  }
+
+  public openCommentEditionModal(demo: IDemo): void {
+    const mode = DemoCommentModalMode.Edition;
+    this.luModal.open(DemoCommentModalComponent, { demo, mode });
+  }
+
+  public openPasswordEditionModal(demo: IDemo): void {
+    this.luModal.open(DemoPasswordEditionModalComponent, demo);
+  }
+
+  private getDemos$(httpParams: HttpParams): Observable<IPaginatedResult<IDemo>> {
+    return this.dataService.getDemos$(httpParams)
+      .pipe(map(data => ({ items: data?.items, totalCount: data.count })));
+  }
+
+  private initDefaultFilter(): void {
+    const principalAsUser = { id: this.principal.id, firstName: this.principal.name, lastName: '' } as IUser;
+    this.filters.setValue({
+      [DemoFilterFormKey.Author]: principalAsUser,
+      [DemoFilterFormKey.IsProtected]: false,
+    });
   }
 
 }
--- front/cc-master/src/app/pages/demos/demos.module.ts
+++ front/cc-master/src/app/pages/demos/demos.module.ts
@@ -1,14 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@cc/aspects/translate';
+import { PagingModule } from '@cc/common/paging';
+import { InstancesDuplicationsDataService } from '@cc/domain/instances';
+import { LuModalModule } from '@lucca-front/ng/modal';
 
+import { DemoCardModule } from './components/demo-card/demo-card.module';
+import { DemoFiltersModule } from './components/demo-filters/demo-filters.module';
+import {
+  DemoCommentModalModule,
+  DemoCreationModalModule,
+  DemoDeletionModalModule,
+  DemoPasswordEditionModalModule,
+} from './components/modals';
 import { DemosComponent } from './demos.component';
+import { ConnectAsDataService } from './services/connect-as-data.service';
+import { DemosApiMappingService } from './services/demos-api-mapping.service';
+import { DemosDataService } from './services/demos-data.service';
+import { DemosDuplicationStoreService } from './services/demos-duplication-store.service';
+import { DemosListService } from './services/demos-list.service';
 
 @NgModule({
-  declarations: [
-    DemosComponent,
-  ],
+  declarations: [DemosComponent],
   imports: [
     CommonModule,
+    DemoFiltersModule,
+    ReactiveFormsModule,
+    DemoCardModule,
+    LuModalModule,
+    DemoDeletionModalModule,
+    DemoCommentModalModule,
+    DemoPasswordEditionModalModule,
+    DemoCreationModalModule,
+    PagingModule,
+    TranslateModule,
+  ],
+  providers: [
+    DemosDataService,
+    DemosApiMappingService,
+    DemosListService,
+    ConnectAsDataService,
+    DemosDuplicationStoreService,
+    InstancesDuplicationsDataService,
   ],
 })
-export class DemosModule { }
+export class DemosModule {
+}
