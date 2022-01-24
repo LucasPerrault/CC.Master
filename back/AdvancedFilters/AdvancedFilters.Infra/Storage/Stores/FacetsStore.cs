27579f3a263b40c5eed688fb1786ddee5afb0e43
Alban Granger <agranger@lucca.fr>
2022-01-24 15:25:11 +0100

    ADD facets store get implementation

--- back/AdvancedFilters/AdvancedFilters.Infra/Storage/Stores/FacetsStore.cs
+++ back/AdvancedFilters/AdvancedFilters.Infra/Storage/Stores/FacetsStore.cs
@@ -1,7 +1,10 @@
 using AdvancedFilters.Domain.Facets;
+using AdvancedFilters.Infra.Storage.DAO;
 using Lucca.Core.Api.Abstractions.Paging;
-using System;
+using Lucca.Core.Api.Queryable.Paging;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdvancedFilters.Infra.Storage.Stores
@@ -9,20 +12,59 @@ namespace AdvancedFilters.Infra.Storage.Stores
     public class FacetsStore : IFacetsStore
     {
         private readonly AdvancedFiltersDbContext _dbContext;
+        private readonly IQueryPager _queryPager;
 
-        public FacetsStore(AdvancedFiltersDbContext dbContext)
+        public FacetsStore(AdvancedFiltersDbContext dbContext, IQueryPager queryPager)
         {
             _dbContext = dbContext;
+            _queryPager = queryPager;
         }
 
         public Task<Page<IEnvironmentFacetValue>> GetAsync(IPageToken pageToken, EnvironmentFacetFilter filter)
         {
-            throw new NotImplementedException();
+            var queryable = EnvironmentValues.ToValues();
+
+            return _queryPager.ToPageAsync(queryable, pageToken);
         }
 
         public Task<List<IEnvironmentFacetValue>> GetAsync(EnvironmentFacetFilter filter)
         {
-            throw new NotImplementedException();
+            return EnvironmentValues.ToValues().ToListAsync();
+        }
+
+        private IQueryable<EnvironmentFacetValueDao> EnvironmentValues => _dbContext.Set<EnvironmentFacetValueDao>();
+    }
+
+    internal static class FacetsStoreExtensions
+    {
+        public static IQueryable<IEnvironmentFacetValue> ToValues(this IQueryable<EnvironmentFacetValueDao> daos)
+        {
+            return daos
+                .Select(dao => dao.ToValue())
+                .Where(v => v != null);
+        }
+
+        public static IEnvironmentFacetValue ToValue(this EnvironmentFacetValueDao dao)
+        {
+            return dao.Facet.Type switch
+            {
+                FacetType.Integer => dao.ToEnvironmentFacetValue(dao.IntValue.Value),
+                FacetType.String => dao.ToEnvironmentFacetValue(dao.StringValue),
+                FacetType.DateTime => dao.ToEnvironmentFacetValue(dao.DateTimeValue.Value),
+                FacetType.Decimal => dao.ToEnvironmentFacetValue(dao.DecimalValue.Value),
+                FacetType.Percentage => dao.ToEnvironmentFacetValue(dao.DecimalValue.Value),
+                _ => null,
+            };
+        }
+        public static IEnvironmentFacetValue ToEnvironmentFacetValue<T>(this EnvironmentFacetValueDao dao, T value)
+        {
+            return new EnvironmentFacetValue<T>
+            {
+                EnvironmentId = dao.EnvironmentId,
+                FacetId = dao.FacetId,
+                Type = dao.Facet.Type,
+                Value = value
+            };
         }
     }
 }
