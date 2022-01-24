cbfc4027f85b0f847af375adf4c6ffab426dff59
Perrault <lperrault@lucca.fr>
2022-01-24 14:27:43 +0100

    add facet store

---
+++ back/AdvancedFilters/AdvancedFilters.Domain/Facets/Facet.cs
@@ -0,0 +1,31 @@
+﻿namespace AdvancedFilters.Domain.Facets;
+
+public enum FacetType
+{
+    Unknown = 0,
+    Integer = 1,
+    DateTime = 2,
+    Decimal = 3,
+    Percentage = 4,
+    String = 5,
+}
+
+public enum FacetScope
+{
+    Unknown = 0,
+    Environment = 1,
+    Establishment = 2,
+}
+
+public class FacetIdentifier
+{
+    public string Code { get; set; }
+    public string ApplicationId { get; set; }
+}
+
+public class Facet : FacetIdentifier
+{
+    public int Id { get; set; }
+    public FacetType Type { get; set; }
+    public FacetScope Scope { get; set; }
+}
---
+++ back/AdvancedFilters/AdvancedFilters.Domain/Facets/FacetValue.cs
@@ -0,0 +1,20 @@
+﻿namespace AdvancedFilters.Domain.Facets;
+
+public interface IFacetValue
+{
+    public int FacetId { get; }
+    public int EnvironmentId { get; }
+}
+
+public interface IEnvironmentFacetValue : IFacetValue
+{
+    public FacetType Type { get; }
+}
+
+public class EnvironmentFacetValue<T> : IEnvironmentFacetValue
+{
+    public T Value { get; set; }
+    public int FacetId { get; set; }
+    public int EnvironmentId { get; set; }
+    public FacetType Type { get; set; }
+}
