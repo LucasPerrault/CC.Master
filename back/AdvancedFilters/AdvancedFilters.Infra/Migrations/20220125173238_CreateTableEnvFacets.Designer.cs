7418f889388e1eaa12a40abc671c3849b95ac217
versinia <angelin.versini@gmail.com>
2022-01-25 18:41:19 +0100

    wip generated files

--- back/AdvancedFilters/AdvancedFilters.Infra/Migrations/20220125173238_CreateTableEnvFacets.Designer.cs
+++ back/AdvancedFilters/AdvancedFilters.Infra/Migrations/20220125173238_CreateTableEnvFacets.Designer.cs
@@ -4,6 +4,7 @@ using AdvancedFilters.Infra.Storage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdvancedFilters.Infra.Migrations
 {
     [DbContext(typeof(AdvancedFiltersDbContext))]
-    partial class AdvancedFiltersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220125173238_CreateTableEnvFacets")]
+    partial class CreateTableEnvFacets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
