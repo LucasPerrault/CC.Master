abc7878ffb8a4548369386723343ca540d32dc94
Perrault <lperrault@lucca.fr>
2022-01-28 18:33:54 +0100

    add establishment list

--- front/cc-master/src/app/pages/cafe/establishments/establishments.component.ts
+++ front/cc-master/src/app/pages/cafe/establishments/establishments.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { defaultPagingParams, IPaginatedResult, PaginatedList, PaginatedListState, PagingService } from '@cc/common/paging';
+import { ApiStandard } from '@cc/common/queries';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { map, skip, takeUntil } from 'rxjs/operators';
+
+import { IEstablishment } from '../common/models/establishment.interface';
+import { EstablishmentsDataService } from './services/establishments-data.service';
 
 @Component({
   selector: 'cc-establishments',
   templateUrl: './establishments.component.html',
   styleUrls: ['./establishments.component.scss'],
 })
-export class EstablishmentsComponent implements OnInit {
+export class EstablishmentsComponent implements OnInit, OnDestroy {
+  public get establishments$(): Observable<IEstablishment[]> { return this.paginatedEts.items$; }
+  public get totalCount$(): Observable<number> { return this.paginatedEts.totalCount$; }
+  public isLoading$ = new ReplaySubject<boolean>(1);
 
-  constructor() { }
+  private paginatedEts: PaginatedList<IEstablishment>;
+  private destroy$: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {
+  constructor(
+    private pagingService: PagingService,
+    private dataService: EstablishmentsDataService,
+  ) {
+    this.paginatedEts = this.pagingService.paginate<IEstablishment>(
+      (httpParams) => this.getEstablishments$(httpParams),
+      { page: defaultPagingParams.page, limit: 50 },
+    );
   }
 
+  public ngOnInit(): void {
+    this.paginatedEts.state$
+      .pipe(takeUntil(this.destroy$), skip(1), map(state => state === PaginatedListState.Update))
+      .subscribe(isLoading => this.isLoading$.next(isLoading));
+
+    this.paginatedEts.updateHttpParams(new HttpParams());
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public nextPage(): void {
+    this.paginatedEts.nextPage();
+  }
+
+  private refresh(): void {
+    this.paginatedEts.updateHttpParams(new HttpParams());
+  }
+
+  private getEstablishments$(httpParams: HttpParams): Observable<IPaginatedResult<IEstablishment>> {
+    return this.dataService.getEstablishments$(httpParams)
+      .pipe(map(response => ({ items: response.items, totalCount: response.count })));
+  }
 }
--- front/cc-master/src/app/pages/cafe/establishments/establishments.module.ts
+++ front/cc-master/src/app/pages/cafe/establishments/establishments.module.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@cc/aspects/translate';
+import { PagingModule } from '@cc/common/paging';
 
+import { EstablishmentListComponent } from './components/establishment-list/establishment-list.component';
 import { EstablishmentsComponent } from './establishments.component';
 import { EstablishmentsConfiguration } from './establishments.configuration';
+import { EstablishmentsDataService } from './services/establishments-data.service';
 
 @NgModule({
-  declarations: [EstablishmentsComponent],
+  declarations: [EstablishmentsComponent, EstablishmentListComponent],
   imports: [
     CommonModule,
+    TranslateModule,
+    PagingModule,
   ],
   exports: [EstablishmentsComponent],
-  providers: [EstablishmentsConfiguration],
+  providers: [EstablishmentsConfiguration, EstablishmentsDataService],
 })
 export class EstablishmentsModule { }
