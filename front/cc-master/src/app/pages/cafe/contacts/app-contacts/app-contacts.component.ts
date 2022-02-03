187a6b6c0acccf258d59451dad388219407e8406
Perrault <lperrault@lucca.fr>
2022-02-03 17:31:06 +0100

    mod contact pages columns selection

--- front/cc-master/src/app/pages/cafe/contacts/app-contacts/app-contacts.component.ts
+++ front/cc-master/src/app/pages/cafe/contacts/app-contacts/app-contacts.component.ts
@@ -36,8 +36,8 @@ export class AppContactsComponent implements OnInit, OnDestroy {
     return this.paginatedContacts.state$.pipe(map(state => state === PaginatedListState.Update));
   }
 
-  public get canExport(): boolean {
-    return !!this.filters?.value?.criterionForms?.length;
+  public get submitDisabled(): boolean {
+    return !this.filters.dirty || !this.filters.valid;
   }
 
   public filters: FormControl = new FormControl();
@@ -67,12 +67,8 @@ export class AppContactsComponent implements OnInit, OnDestroy {
   }
 
   public ngOnInit(): void {
-    this.advancedFilter$
-      .pipe(takeUntil(this.destroy$), filter(f => !!f))
-      .subscribe(() => this.refresh());
-
     this.filters.valueChanges
-      .pipe(takeUntil(this.destroy$), this.toAdvancedFilter)
+      .pipe(takeUntil(this.destroy$), filter(() => !this.submitDisabled), this.toAdvancedFilter)
       .subscribe(advancedFilter => this.advancedFilter$.next(advancedFilter));
   }
 
@@ -85,14 +81,14 @@ export class AppContactsComponent implements OnInit, OnDestroy {
     this.paginatedContacts.nextPage();
   }
 
+  public submit(): void {
+    this.paginatedContacts.updateHttpParams(new HttpParams());
+  }
+
   public export(): void {
     this.contactsService.exportAppContacts$(this.advancedFilter$.value)
       .pipe(take(1), toSubmissionState(), map(state => getButtonState(state)))
       .subscribe(c => this.exportButtonClass$.next(c));
-  }
-
-  private refresh(): void {
-    this.paginatedContacts.updateHttpParams(new HttpParams());
   }
 
   private getPaginatedAppContacts$(): PaginatedList<IAppContact> {
