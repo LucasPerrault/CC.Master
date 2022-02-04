2c1a61f6be4149735d4e1d8ac8c582069153c210
Perrault <lperrault@lucca.fr>
2022-02-04 18:40:19 +0100

    add count code migration

---
+++ back/Billing/Billing.Contracts/Billing.Contracts.Infra/Migrations/20220202104341_AlterCountsAddCountCodeColumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Billing.Contracts.Infra.Migrations
+{
+    public partial class AlterCountsAddCountCodeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                $@"
+                ALTER VIEW [billing].[Counts] WITH SCHEMABINDING AS
+                    SELECT
+                        c.idCount Id,
+                        c.idContract ContractId,
+                        c.idCommercialOffer CommercialOfferId,
+                        c.countPeriod CountPeriod,
+                        CAST(CASE WHEN c.Code = 'COUNT' THEN 0 ELSE 1 END AS int) AS Code
+                FROM [dbo].[Counts] c
+                    LEFT JOIN [dbo].[Contracts] co ON co.idContract = c.idContract
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                $@"
+                ALTER VIEW [billing].[Counts] WITH SCHEMABINDING AS
+                    SELECT
+                        c.idCount Id,
+                        c.idContract ContractId,
+                        c.idCommercialOffer CommercialOfferId,
+                        c.countPeriod CountPeriod
+                FROM [dbo].[Counts] c
+                    LEFT JOIN [dbo].[Contracts] co ON co.idContract = c.idContract
+            ");
+        }
+    }
+}
