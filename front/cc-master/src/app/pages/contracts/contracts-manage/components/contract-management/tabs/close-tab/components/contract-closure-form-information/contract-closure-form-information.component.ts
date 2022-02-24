c691d992992425f0bdd29eb5b0107fef94b2e652
Perrault <lperrault@lucca.fr>
2022-02-24 15:03:13 +0100

    fix close contract min date reason

--- front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/close-tab/components/contract-closure-form-information/contract-closure-form-information.component.ts
+++ front/cc-master/src/app/pages/contracts/contracts-manage/components/contract-management/tabs/close-tab/components/contract-closure-form-information/contract-closure-form-information.component.ts
@@ -1,8 +1,9 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslatePipe } from '@cc/aspects/translate';
+import { isAfter } from 'date-fns';
 
-import { IClosureFormValidationContext, IContextAttachment } from '../../models/closure-form-validation-context.interface';
+import { IClosureFormValidationContext } from '../../models/closure-form-validation-context.interface';
 
 @Component({
   selector: 'cc-contract-closure-form-information',
@@ -13,28 +14,57 @@ export class ContractClosureFormInformationComponent {
 
   constructor(private translatePipe: TranslatePipe, private datePipe: DatePipe) { }
 
-  public getAttachmentDateInformation(attachment: IContextAttachment): string {
-    const hasEndDate = !!attachment?.end;
-    const mostRecentDate = hasEndDate ? attachment.end : attachment.start;
-    const translationKey = hasEndDate ? 'contracts_closeDate_blockingAttachment_until' : 'contracts_closeDate_blockingAttachment_since';
+  public getMinDateReason(): string {
+    const attachmentDate = this.getAttachmentDate();
+
+    if (!!this.context?.mostRecentAttachment && !!this.context?.lastCountPeriod) {
+      return isAfter(attachmentDate, this.context.lastCountPeriod)
+        ? this.getAttachmentDateInformation(attachmentDate)
+        : this.getLastCountDateInformation();
+    }
+
+    if (!!this.context?.mostRecentAttachment) {
+      return this.getAttachmentDateInformation(attachmentDate);
+    }
+
+    if (!!this.context.lastCountPeriod) {
+      return this.getLastCountDateInformation();
+    }
+
+    return this.getTheoreticalStartOnInformation();
+  }
+
+  private getAttachmentDateInformation(attachmentDate: Date): string {
+    const translationKey = !!this.context.mostRecentAttachment?.end
+      ? 'contracts_closeDate_blockingAttachment_until'
+      : 'contracts_closeDate_blockingAttachment_since';
 
     return this.translatePipe.transform(translationKey, {
-      name: attachment.legalEntity.name,
-      id: attachment.legalEntity.id,
-      date: this.datePipe.transform(mostRecentDate, 'MMMM yyyy'),
+      name: this.context.mostRecentAttachment.legalEntity.name,
+      id: this.context.mostRecentAttachment.legalEntity.id,
+      date: this.datePipe.transform(attachmentDate, 'MMMM yyyy'),
     });
   }
 
-  public getLastCountDateInformation(): string {
+  private getLastCountDateInformation(): string {
     const lastCountDate = this.datePipe.transform(this.context.lastCountPeriod, 'MMMM yyyy');
     const translationKey = 'front_contractPage_closeDateCondition_lastCountDate_callout';
     return this.translatePipe.transform(translationKey, { date: lastCountDate });
   }
 
-  public getTheoreticalStartOnInformation(): string {
+  private getTheoreticalStartOnInformation(): string {
     const theoreticalStartDate = this.datePipe.transform(this.context.theoreticalStartOn, 'MMMM yyyy');
     const translationKey = 'front_contractPage_closeDateCondition_theoreticalStartOn_callout';
     return this.translatePipe.transform(translationKey, { date: theoreticalStartDate });
+  }
+
+  private getAttachmentDate(): Date {
+    if (!this.context.mostRecentAttachment) {
+      return null;
+    }
+
+    const hasEndDate = !!this.context.mostRecentAttachment?.end;
+    return hasEndDate? new Date(this.context.mostRecentAttachment.end) : new Date(this.context.mostRecentAttachment.start);
   }
 
 }
