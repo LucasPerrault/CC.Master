883c3e9b93d3aedda15aa8f2acf3c179dba79084
Guillaume Mouron <gmouron@lucca.fr>
2022-02-28 14:19:14 +0100

    [MOD] Les entrées DNS sont toujours forcées en minuscule

--- back/Instances/Instances.Domain.Tests/Instances/DnsEntryTests.cs
+++ back/Instances/Instances.Domain.Tests/Instances/DnsEntryTests.cs
@@ -34,6 +34,62 @@ namespace Instances.Domain.Tests.Instances
             act.Should().Throw<NotSupportedException>();
         }
 
+        [Fact]
+        public void ForProduction_AlwaysLowercaseDnsEntry()
+        {
+            var subDomain = "TEST";
+            var cluster = "cluster1";
+            var entry = DnsEntry.ForProduction(subDomain, cluster, EnvironmentDomain.ILuccaDotNet);
+
+            Assert.Equal("test", entry.Subdomain);
+        }
+        #endregion
+
+        #region ForDemo
+        [Fact]
+        public void ForDemo_AlwaysLowercaseDnsEntry()
+        {
+            var subDomain = "TEST";
+            var cluster = "cluster1";
+            var entry = DnsEntry.ForDemo(subDomain, cluster);
+
+            Assert.Equal("test", entry.Subdomain);
+        }
+        #endregion
+
+        #region ForPreview
+        [Fact]
+        public void ForPreview_AlwaysLowercaseDnsEntry()
+        {
+            var subDomain = "TEST";
+            var cluster = "cluster1";
+            var entry = DnsEntry.ForPreview(subDomain, cluster);
+
+            Assert.Equal("test", entry.Subdomain);
+        }
+        #endregion
+
+        #region ForTraining
+        [Fact]
+        public void ForTraining_AlwaysLowercaseDnsEntry()
+        {
+            var subDomain = "TEST";
+            var cluster = "cluster1";
+            var entry = DnsEntry.ForTraining(subDomain, cluster);
+
+            Assert.Equal("test", entry.Subdomain);
+        }
+        #endregion
+
+        #region ForRedirection
+        [Fact]
+        public void ForRedirection_AlwaysLowercaseDnsEntry()
+        {
+            var subDomain = "TEST";
+            var entry = DnsEntry.ForRedirection(subDomain);
+
+            Assert.Equal("test", entry.Subdomain);
+        }
         #endregion
     }
 }
