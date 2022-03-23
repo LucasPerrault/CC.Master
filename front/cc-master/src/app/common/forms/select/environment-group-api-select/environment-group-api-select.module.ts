9b374aa9bed8bf13c7bf1f2b93d8def4c6945b63
Perrault <lperrault@lucca.fr>
2022-03-23 12:13:30 +0100

    add search in environment group select

--- front/cc-master/src/app/common/forms/select/environment-group-api-select/environment-group-api-select.module.ts
+++ front/cc-master/src/app/common/forms/select/environment-group-api-select/environment-group-api-select.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@cc/aspects/translate';
-import { LuApiFeederModule } from '@lucca-front/ng/api';
+import { LuApiFeederModule, LuApiSearcherModule } from '@lucca-front/ng/api';
 import { LuInputClearerModule, LuInputDisplayerModule } from '@lucca-front/ng/input';
 import { LuForOptionsModule, LuOptionItemModule, LuOptionPickerModule, LuOptionSelectAllModule } from '@lucca-front/ng/option';
 import { LuSelectInputModule } from '@lucca-front/ng/select';
@@ -25,7 +25,9 @@ import { EnvironmentGroupApiSelectComponent } from './environment-group-api-sele
     LuOptionItemModule,
     LuInputClearerModule,
     LuForOptionsModule,
+    LuApiSearcherModule,
   ],
   exports: [EnvironmentGroupApiSelectComponent],
 })
-export class EnvironmentGroupApiSelectModule { }
+export class EnvironmentGroupApiSelectModule {
+}
